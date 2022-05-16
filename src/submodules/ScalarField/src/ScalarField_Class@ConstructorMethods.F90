3e62d83fb3ed064fc163dfdad10487f210ac421f
Vikas Sharma <Vickysharma0812@gmail.com>
2022-05-17 08:49:07 +0900

    Minor changes

--- src/submodules/ScalarField/src/ScalarField_Class@ConstructorMethods.F90
+++ src/submodules/ScalarField/src/ScalarField_Class@ConstructorMethods.F90
@@ -15,7 +15,7 @@
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 !
 
-SUBMODULE(ScalarField_Class) Constructor
+SUBMODULE(ScalarField_Class) ConstructorMethods
 USE BaseMethod
 IMPLICIT NONE
 CONTAINS
@@ -34,7 +34,6 @@ MODULE PROCEDURE setScalarFieldParam
     ierr = param%set( key="ScalarField/fieldType", value=FIELD_TYPE_NORMAL )
   END IF
 END PROCEDURE setScalarFieldParam
-
 
 !----------------------------------------------------------------------------
 !
@@ -74,28 +73,39 @@ MODULE PROCEDURE sField_Initiate1
   INTEGER( I4B ) :: tNodes( 1 ), spaceCompo( 1 ), timeCompo( 1 )
   CHARACTER( LEN=: ), ALLOCATABLE :: char_var
   CHARACTER( LEN=1 ) :: names_char( 1 )
-
-  !> main program
+  !!
+  !! main program
+  !!
   IF( obj%isInitiated ) &
     & CALL e%raiseError(modName//'::'//myName// " - "// &
     & 'Scalar object is already initiated')
-  !> check
+  !!
+  !! check
+  !!
   CALL obj%checkEssentialParam(param)
-  !> READ name
+  !!
+  !! name
+  !!
   ALLOCATE( CHARACTER( LEN = &
     & param%DataSizeInBytes( key="ScalarField/name" ) ) :: char_var )
   ierr = param%get( key="ScalarField/name", value=char_var )
   obj%name = char_var
   names_char( 1 )(1:1) = char_var( 1:1 )
-  !> READ fieldType
+  !!
+  !! fieldType
+  !!
   IF( param%isPresent(key="ScalarField/fieldType") ) THEN
     ierr = param%get( key="ScalarField/fieldType", value=obj%fieldType )
   ELSE
     obj%fieldType = FIELD_TYPE_NORMAL
   END IF
-  !> SET engine
+  !!
+  !! engine
+  !!
   obj%engine="NATIVE_SERIAL"
-  !
+  !!
+  !!
+  !!
   spaceCompo = [1]
   timeCompo = [1]
   storageFMT = FMT_NODES
@@ -107,38 +117,21 @@ MODULE PROCEDURE sField_Initiate1
     tNodes = obj%domain%getTotalNodes()
     obj%tSize = tNodes( 1 )
   END IF
-  !
+  !!
+  !!
+  !!
   CALL initiate( obj=obj%dof, tNodes=tNodes, names=names_char, &
     & spaceCompo=spaceCompo, timeCompo=timeCompo, storageFMT=storageFMT )
-  !>
+  !!
+  !!
+  !!
   CALL initiate( obj%realVec, obj%dof )
-  !>
+  !!
   obj%isInitiated = .TRUE.
-  !>
+  !!
   IF( ALLOCATED( char_var ) ) DEALLOCATE( char_var )
+  !!
 END PROCEDURE sField_Initiate1
-
-!----------------------------------------------------------------------------
-!                                                                 Initiate
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE sField_Initiate2
-  CHARACTER( LEN = * ), PARAMETER :: myName="sField_Initiate2"
-  IF( .NOT. obj2%isInitiated ) &
-    & CALL e%raiseError(modName//'::'//myName// " - "// &
-    & 'Obj2 is not initiated!')
-  obj%isInitiated = .TRUE.
-  obj%fieldType = obj2%fieldType
-  obj%domain => obj2%domain
-  obj%name = obj2%name
-  obj%engine = obj2%engine
-  SELECT TYPE ( obj2 )
-  TYPE IS ( ScalarField_ )
-    obj%tSize = obj2%tSize
-    obj%realVec = obj2%realVec
-    obj%dof = obj2%dof
-  END SELECT
-END PROCEDURE sField_Initiate2
 
 !----------------------------------------------------------------------------
 !                                                             Deallocate
@@ -177,4 +170,4 @@ END PROCEDURE sField_Constructor_1
 !
 !----------------------------------------------------------------------------
 
-END SUBMODULE Constructor
+END SUBMODULE ConstructorMethods
