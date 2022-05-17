57e4ee552b4f4a16288f6bebbc9219f34d309201
Vikas Sharma <Vickysharma0812@gmail.com>
2022-05-17 19:35:50 +0900

    Adding mesh selection methods

---
+++ src/submodules/MeshSelection/src/MeshSelection_Class@GetElemNumMethods.F90
@@ -0,0 +1,156 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+
+!> authors: Vikas Sharma, Ph. D.
+! date: 28 Aug 2021
+! summary: This module defines a data type for mesh selection
+
+SUBMODULE(MeshSelection_Class) GetElemNumMethods
+USE BaseMethod
+USE Mesh_Class, ONLY: Mesh_
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                getElemNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getElemNum1
+  SELECT CASE (dim)
+  CASE (0)
+    IF (isAllocated(obj%PointElemNum)) ans = obj%PointElemNum
+  CASE (1)
+    IF (isAllocated(obj%CurveElemNum)) ans = obj%CurveElemNum
+  CASE (2)
+    IF (isAllocated(obj%SurfaceElemNum)) ans = obj%SurfaceElemNum
+  CASE (3)
+    IF (isAllocated(obj%VolumeElemNum)) ans = obj%VolumeElemNum
+  END SELECT
+  IF (.NOT. ALLOCATED(ans)) ALLOCATE (ans(0))
+END PROCEDURE meshSelect_getElemNum1
+
+!----------------------------------------------------------------------------
+!                                                                 getElemNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getElemNum2
+  CHARACTER(len=*), PARAMETER :: myname = "meshSelect_getElemNum2"
+  CLASS(Mesh_), POINTER :: meshptr
+  INTEGER(I4B) :: ii
+  !!
+  !! isSelectionByElemNum
+  !!
+  IF (obj%isSelectionByElemNum) THEN
+    ans = obj%getElemNum(dim=dim)
+  END IF
+  !!
+  !! isSelectionByMeshID
+  !!
+  IF (obj%isSelectionByMeshID) THEN
+    !!
+    meshptr => NULL()
+    !!
+    SELECT CASE (dim)
+    !!
+    !!
+    !!
+    !!
+    CASE (0)
+      !!
+      DO ii = 1, SIZE(obj%pointMeshID)
+        meshptr => domain%getMeshPointer( &
+          & dim=dim, &
+          & entityNum=obj%pointMeshID%val(ii))
+        CALL append(ans, meshptr%getElemNum())
+      END DO
+    !!
+    !!
+    !!
+    !!
+    CASE (1)
+      !!
+      DO ii = 1, SIZE(obj%curveMeshID)
+        meshptr => domain%getMeshPointer(dim=dim, &
+          & entityNum=obj%curveMeshID%val(ii))
+        CALL append(ans, meshptr%getElemNum())
+      END DO
+    !!
+    !!
+    !!
+    !!
+    CASE (2)
+      !!
+      DO ii = 1, SIZE(obj%surfaceMeshID)
+        meshptr => domain%getMeshPointer(dim=dim, &
+          & entityNum=obj%surfaceMeshID%val(ii))
+        CALL append(ans, meshptr%getElemNum())
+      END DO
+    !!
+    !!
+    !!
+    !!
+    CASE (3)
+      !!
+      DO ii = 1, SIZE(obj%volumeMeshID)
+        meshptr => domain%getMeshPointer(dim=dim, &
+          & entityNum=obj%volumeMeshID%val(ii))
+        CALL append(ans, meshptr%getElemNum())
+      END DO
+    !!
+    !!
+    !!
+    !!
+    END SELECT
+    !!
+  END IF
+  !!
+  !!
+  !!
+  IF (.NOT. ALLOCATED(ans)) ALLOCATE (ans(0))
+  ! TODO enhance getElemNum in [[MeshSelection_]] so that it works
+  ! when isSelectionByNodeNum and isSelectionByBox is true.
+END PROCEDURE meshSelect_getElemNum2
+
+!----------------------------------------------------------------------------
+!                                                                getElemNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getElemNum3
+  INTEGER(I4B) :: ii
+  DO ii = 0, 3
+    CALL append(ans, obj%getElemNum(dim=ii))
+  END DO
+END PROCEDURE meshSelect_getElemNum3
+
+!----------------------------------------------------------------------------
+!                                                                getElemNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getElemNum4
+  !!
+  INTEGER(I4B) :: ii
+  INTEGER(I4B), ALLOCATABLE :: intvec(:)
+  !!
+  !! main
+  !!
+  DO ii = 0, 3
+    intvec = obj%getElemNum(dim=ii, domain=domain)
+    CALL append(ans, intvec)
+  END DO
+  !!
+END PROCEDURE meshSelect_getElemNum4
+
+END SUBMODULE GetElemNumMethods
---
+++ src/submodules/MeshSelection/src/MeshSelection_Class@GetNodeNumMethods.F90
@@ -0,0 +1,168 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+
+!> authors: Vikas Sharma, Ph. D.
+! date: 28 Aug 2021
+! summary: This module defines a data type for mesh selection
+
+SUBMODULE(MeshSelection_Class) GetNodeNumMethods
+USE BaseMethod
+USE Mesh_Class, ONLY: Mesh_
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                getNodeNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getNodeNum1
+  IF (isAllocated(obj%NodeNum)) ans = obj%NodeNum
+END PROCEDURE meshSelect_getNodeNum1
+
+!----------------------------------------------------------------------------
+!                                                                getNodeNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getNodeNum2
+  TYPE(IntVector_) :: aintvec
+  INTEGER( I4B ), ALLOCATABLE :: indx(:)
+  INTEGER( I4B ) :: ii
+  !!
+  !! isSelectionByNodeNum
+  !!
+  IF (obj%isSelectionByNodeNum) THEN
+    CALL APPEND(aintvec, obj%getNodeNum())
+  END IF
+  !!
+  !! isSelectionByMeshID
+  !!
+  IF (obj%isSelectionByMeshID) THEN
+    !!
+    IF (obj%isMeshIDAllocated(dim=dim)) THEN
+      !!
+      indx = obj%getMeshID(dim=dim)
+      !!
+      CALL APPEND( &
+        & aintvec, &
+        & domain%getNptrs(dim=dim, entityNum=indx))
+      !!
+      IF (ALLOCATED(indx)) DEALLOCATE (indx)
+      !!
+    END IF
+    !!
+  END IF
+  !!
+  !! isSelectionByElemNum
+  !!
+  IF (obj%isSelectionByElemNum) THEN
+    !!
+    indx = obj%getElemNum(dim=dim)
+    !!
+    DO ii = 1, SIZE( indx )
+      !!
+      CALL APPEND( &
+        & aintvec, &
+        & domain%getConnectivity( globalElement=indx(ii) ))
+      !!
+    END DO
+    !!
+    IF( ALLOCATED( indx ) ) DEALLOCATE( indx )
+    !!
+  END IF
+  !!
+  IF ( isAllocated(aIntVec)) THEN
+    CALL RemoveDuplicates( aIntVec )
+    ans = aIntVec
+    CALL Deallocate(aIntVec)
+  END IF
+  !!
+END PROCEDURE meshSelect_getNodeNum2
+
+!----------------------------------------------------------------------------
+!                                                                getNodeNum
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE meshSelect_getNodeNum3
+  CHARACTER( LEN = * ), PARAMETER :: myName="meshSelect_getNodeNum3"
+  TYPE(IntVector_) :: aintvec
+  INTEGER( I4B ), ALLOCATABLE :: indx(:), nptrs( : )
+  INTEGER( I4B ) :: ii, dim, nsd
+  !!
+  !! isSelectionByNodeNum
+  !!
+  IF (obj%isSelectionByNodeNum) THEN
+    nptrs = obj%getNodeNum( )
+    IF( ALLOCATED( nptrs ) ) THEN
+      CALL APPEND(aintvec, nptrs )
+      DEALLOCATE( nptrs )
+    END IF
+  END IF
+  !!
+  !! isSelectionByMeshID
+  !!
+  IF (obj%isSelectionByMeshID) THEN
+    !!
+    nsd = domain%getNSD()
+    !!
+    DO dim = 0, nsd
+      !!
+      IF (obj%isMeshIDAllocated(dim=dim)) THEN
+        !!
+        indx = obj%getMeshID(dim=dim)
+        !!
+        nptrs = domain%getNptrs(dim=dim, entityNum=indx)
+        IF( ALLOCATED( nptrs ) ) THEN
+          CALL APPEND( aintvec, nptrs )
+          DEALLOCATE( nptrs )
+        END IF
+        !!
+        IF (ALLOCATED(indx)) DEALLOCATE (indx)
+        !!
+      END IF
+      !!
+    END DO
+    !!
+  END IF
+  !!
+  !! isSelectionByElemNum
+  !!
+  IF (obj%isSelectionByElemNum) THEN
+    !!
+    indx = obj%getElemNum()
+    !!
+    DO ii = 1, SIZE( indx )
+      !!
+      CALL APPEND( &
+        & aintvec, &
+        & domain%getConnectivity( globalElement=indx(ii) ))
+      !!
+    END DO
+    !!
+    IF( ALLOCATED( indx ) ) DEALLOCATE( indx )
+    !!
+  END IF
+  !!
+  IF ( isAllocated(aIntVec)) THEN
+    CALL RemoveDuplicates( aIntVec )
+    ans = aIntVec
+    CALL Deallocate(aIntVec)
+  END IF
+  !!
+END PROCEDURE meshSelect_getNodeNum3
+
+END SUBMODULE GetNodeNumMethods
+
+
