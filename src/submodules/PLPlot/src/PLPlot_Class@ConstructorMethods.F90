0357b19f673ff53b149da0488d77ed13ad65edfd
Vikas Sharma <Vickysharma0812@gmail.com>
2022-07-24 18:22:06 +0900

    plplot

---
+++ src/submodules/PLPlot/src/PLPlot_Class@ConstructorMethods.F90
@@ -0,0 +1,47 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(PLPlot_Class) ConstructorMethods
+USE BaseMethod
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                 Initiate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE plot_Initiate
+  obj%plotEngine=PLOT_ENGINE_PLPLOT
+END PROCEDURE plot_Initiate
+
+!----------------------------------------------------------------------------
+!                                                                 Deallocate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE plot_Deallocate
+  obj%plotEngine=PLOT_ENGINE_PLPLOT
+END PROCEDURE plot_Deallocate
+
+!----------------------------------------------------------------------------
+!                                                                 Display
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE plot_Display
+  CALL Display( "# PLOT ENGINE : PLPLOT", msg, unitno)
+END PROCEDURE plot_Display
+
+END SUBMODULE ConstructorMethods
