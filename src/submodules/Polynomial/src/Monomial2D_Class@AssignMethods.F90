12bf4a020f43162c2938a3de99814d4a4305e851
Vikas Sharma <Vickysharma0812@gmail.com>
2022-07-27 12:11:27 +0900

    polynomial

---
+++ src/submodules/Polynomial/src/Monomial2D_Class@AssignMethods.F90
@@ -0,0 +1,33 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(Monomial2D_Class) AssignMethods
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE func_AssignObjObj
+  obj%x(1)=obj2%x(1)
+  obj%x(2)=obj2%x(2)
+  obj%coeff = obj2%coeff
+  obj%uid = obj2%uid
+END PROCEDURE func_AssignObjObj
+
+END SUBMODULE AssignMethods
