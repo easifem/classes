bc30f8fba5cb235210d8dec3e6f59a3ea21f0c2a
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-05 21:05:47 +0900

    working on polynomials

---
+++ src/submodules/Monomial3D/src/Monomial3D_Class@OperatorMethods.F90
@@ -0,0 +1,49 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBMODULE(Monomial3D_Class) OperatorMethods
+USE BaseMethod
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                            Multiplication
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE func_Multiplication_obj_obj
+  INTEGER( I4B ) :: n1, n2, n3
+  !!
+  n1 = obj1%x(1)%getDegree() + obj2%x(1)%getDegree()
+  n2 = obj1%x(2)%getDegree() + obj2%x(2)%getDegree()
+  n3 = obj1%x(3)%getDegree() + obj2%x(3)%getDegree()
+  !!
+  ans=Monomial3D(n1=n1, n2=n2, n3=n3, &
+    & name1=obj1%varname(1)%chars(), &
+    & name2=obj1%varname(2)%chars(), &
+    & name3=obj1%varname(3)%chars() )
+  !!
+END PROCEDURE func_Multiplication_obj_obj
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+END SUBMODULE OperatorMethods
