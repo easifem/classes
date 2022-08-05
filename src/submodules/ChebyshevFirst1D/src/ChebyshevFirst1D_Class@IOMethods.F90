cb862fedbb8d39a63132b5b059c91e777bfce640
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-05 21:24:08 +0900

    renamed Chebyshev class

--- src/submodules/ChebyshevFirst1D/src/ChebyshevFirst1D_Class@IOMethods.F90
+++ src/submodules/ChebyshevFirst1D/src/ChebyshevFirst1D_Class@IOMethods.F90
@@ -15,5 +15,5 @@
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 !
 
-SUBMODULE(Chebyshev1Polynomial1D_Class) IOMethods
+SUBMODULE(ChebyshevFirst1D_Class) IOMethods
 END SUBMODULE IOMethods
