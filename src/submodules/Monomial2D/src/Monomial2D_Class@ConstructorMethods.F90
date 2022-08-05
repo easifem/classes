eca829b721a916370a7b30e42734741cd09c6042
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-05 21:05:40 +0900

    working on polynomials

--- src/submodules/Monomial2D/src/Monomial2D_Class@ConstructorMethods.F90
+++ src/submodules/Monomial2D/src/Monomial2D_Class@ConstructorMethods.F90
@@ -29,7 +29,7 @@ CONTAINS
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE func_Deallocate
-  CALL AbstractMonomialDeallocate( obj )
+  CALL AbstractBasis2DDeallocate( obj )
   CALL obj%x(1)%Deallocate()
   CALL obj%x(2)%Deallocate()
 END PROCEDURE func_Deallocate
@@ -50,9 +50,10 @@ MODULE PROCEDURE func_Monomial2D1
   !!
   TYPE( String ) :: astr
   !!
-  ans%coeff = coeff
-  ans%x(1) = Monomial1D(coeff=1.0_DFP, degree=degree(1), varname=varname(1))
-  ans%x(2) = Monomial1D(coeff=1.0_DFP, degree=degree(2), varname=varname(2))
+  ans%x(1) = Monomial1D(degree=n1, varname=name1)
+  ans%x(2) = Monomial1D(degree=n2, varname=name2)
+  ans%varname(1) = TRIM(name1)
+  ans%varname(2) = TRIM(name2)
   astr = ans%GetStringForUID( )
   ans%uid = StringToUID(astr%chars())
   !!
@@ -64,39 +65,17 @@ END PROCEDURE func_Monomial2D1
 
 MODULE PROCEDURE func_Monomial2D2
   !!
-  REAL( DFP ) :: coeff0
-  INTEGER( I4B ) :: degree(2)
-  CHARACTER( LEN = 256 ) :: varname(2)
+  INTEGER( I4B ) :: n1, n2
+  CHARACTER( LEN = 256 ) :: name1, name2
   !!
-  coeff0 = coeff * f1%coeff * f2%coeff
-  degree(1) = f1%degree
-  degree(2) = f2%degree
-  varname(1) = f1%varname%chars()
-  varname(2) = f2%varname%chars()
+  n1 = f1%GetDegree()
+  n2 = f2%GetDegree()
+  name1 = f1%varname%chars()
+  name2 = f2%varname%chars()
   !!
-  ans = Monomial2D(coeff=coeff, degree=degree, varname=varname)
+  ans = Monomial2D(n1=n1, n2=n2, name1=name1, name2=name2)
   !!
 END PROCEDURE func_Monomial2D2
-
-!----------------------------------------------------------------------------
-!                                                               Monomial2D
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE func_Monomial2D_Pointer2
-  !!
-  REAL( DFP ) :: coeff0
-  INTEGER( I4B ) :: degree(2)
-  CHARACTER( LEN = 256 ) :: varname(2)
-  !!
-  coeff0 = coeff * f1%coeff * f2%coeff
-  degree(1) = f1%degree
-  degree(2) = f2%degree
-  varname(1) = f1%varname%chars()
-  varname(2) = f2%varname%chars()
-  !!
-  ans => Monomial2D_Pointer(coeff=coeff, degree=degree, varname=varname)
-  !!
-END PROCEDURE func_Monomial2D_Pointer2
 
 !----------------------------------------------------------------------------
 !                                                         Monomial2D_Pointer
@@ -106,12 +85,35 @@ MODULE PROCEDURE func_Monomial2D_Pointer1
   TYPE( String ) :: astr
   !!
   ALLOCATE( Monomial2D_::ans )
-  ans%coeff = coeff
-  ans%x(1) = Monomial1D(coeff=1.0_DFP, degree=degree(1), varname=varname(1))
-  ans%x(2) = Monomial1D(coeff=1.0_DFP, degree=degree(2), varname=varname(2))
+  ans%x(1) = Monomial1D(degree=n1, varname=name1)
+  ans%x(2) = Monomial1D(degree=n2, varname=name2)
+  ans%varname(1) = TRIM(name1)
+  ans%varname(2) = TRIM(name2)
   astr = ans%GetStringForUID( )
   ans%uid = StringToUID(astr%chars())
   !!
 END PROCEDURE func_Monomial2D_Pointer1
+
+!----------------------------------------------------------------------------
+!                                                               Monomial2D
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE func_Monomial2D_Pointer2
+  !!
+  INTEGER( I4B ) :: n1, n2
+  CHARACTER( LEN = 256 ) :: name1, name2
+  !!
+  n1 = f1%GetDegree()
+  n2 = f2%GetDegree()
+  name1 = f1%varname%chars()
+  name2 = f2%varname%chars()
+  !!
+  ans => Monomial2D_Pointer(n1=n1, n2=n2, name1=name1, name2=name2)
+  !!
+END PROCEDURE func_Monomial2D_Pointer2
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END SUBMODULE ConstructorMethods
