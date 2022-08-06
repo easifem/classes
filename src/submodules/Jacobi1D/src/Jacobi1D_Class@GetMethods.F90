d3f4f9df7fd62f9801a1c51d7e8942493a69ecfa
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-06 14:01:53 +0900

    :wo orthopol

--- src/submodules/Jacobi1D/src/Jacobi1D_Class@GetMethods.F90
+++ src/submodules/Jacobi1D/src/Jacobi1D_Class@GetMethods.F90
@@ -21,198 +21,170 @@ IMPLICIT NONE
 CONTAINS
 
 !----------------------------------------------------------------------------
-!                                                                 Eval
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE J_EvalScalar
-REAL( DFP ) :: Jn_1, Jn_2
-SELECT CASE( obj%n )
-CASE( -1_I4B )
-  ans = 0.0_DFP
-CASE( 0_I4B )
-  ans = 1.0_DFP
-CASE DEFAULT
-  !!
-  IF( ASSOCIATED( obj%Jn_1 ) ) THEN
-    Jn_1 = obj%Jn_1%Eval( x )
-  ELSE
-    Jn_1 = 0.0_DFP
-  END IF
-  !!
-  IF( ASSOCIATED( obj%Jn_2 ) ) THEN
-    Jn_2 = obj%Jn_2%Eval( x )
-  ELSE
-    Jn_2 = 0.0_DFP
-  END IF
-  !!
-  ans = (x-obj%an_1)*obj%sn_1*Jn_1 - obj%bn_1*obj%sn_2*Jn_2
-  !!
-END SELECT
-END PROCEDURE J_EvalScalar
-
-!----------------------------------------------------------------------------
-!                                                              EvalGradient
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE J_EvalGradientScalar
-REAL( DFP ) :: GJn_1, GJn_2, Jn_1
-SELECT CASE( obj%n )
-CASE( -1_I4B, 0_I4B )
-  ans = 0.0_DFP
-CASE DEFAULT
-  !!
-  IF( ASSOCIATED( obj%Jn_1 ) ) THEN
-    GJn_1 = obj%Jn_1%EvalGradient( x )
-    Jn_1 = obj%Jn_1%Eval( x )
-  ELSE
-    GJn_1 = 0.0_DFP
-    Jn_1 = 0.0_DFP
-  END IF
-  !!
-  IF( ASSOCIATED( obj%Jn_2 ) ) THEN
-    GJn_2 = obj%Jn_2%EvalGradient( x )
-  ELSE
-    GJn_2 = 0.0_DFP
-  END IF
-  !!
-  ans = (x-obj%an_1) * obj%sn_1 * GJn_1 &
-    & - obj%bn_1 * obj%sn_2 * GJn_2 &
-    & + obj%sn_1 * Jn_1
-  !!
-END SELECT
-!!
-END PROCEDURE J_EvalGradientScalar
-
-!----------------------------------------------------------------------------
 !                                                           GetStringForUID
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_GetStringForUID
-  ans = "J" // TRIM( STR( obj%n ) ) // obj%varname%chars()
-END PROCEDURE J_GetStringForUID
-
-!----------------------------------------------------------------------------
-!                                                         GetStringToDisplay
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE J_GetStringToDisplay
-  INTEGER( I4B ) :: n_1, n_2
-  !!
-  IF( obj%n .EQ. 0 ) THEN
-    ans = "J_0( " // obj%varname%chars() // " ) = 1"
-  ELSEIF( obj%n .EQ. -1 ) THEN
-    ans = ""
-  ELSEIF( obj%n .EQ. 1 ) THEN
-    ans = "J_1( " // obj%varname%chars() // " ) = " // &
-    & "( x-"// TRIM(STR( n=obj%an_1, fm="(f0.3)")) // " )* " // &
-    & TRIM(STR( n=obj%sn_1, fm="(f0.3)")) // "* J_0"
-  ELSE
-    !!
-    IF( ASSOCIATED( obj%Jn_1 ) ) THEN
-      n_1 = obj%Jn_1%n
-    ELSE
-      n_1 = 0
-    END IF
-    !!
-    IF( ASSOCIATED( obj%Jn_2 ) ) THEN
-      n_2 = obj%Jn_2%n
-    ELSE
-      n_2 = 0
-    END IF
-    !!
-    ans = "J_" // TRIM( STR( obj%n ) ) // "( " //  &
-      & obj%varname%chars() // " ) = " //&
-      &  "( x-"// TRIM(STR( n=obj%an_1, fm="(f0.3)")) //&
-      & " )* " // TRIM(STR( n=obj%sn_1, fm="(f0.3)")) //&
-      &  "* J_" // &
-      & TRIM( STR( n_1 ) ) // &
-      & "( " //  obj%varname%chars() // " ) - " // &
-      & TRIM( STR( n=obj%bn_1, fm="(f0.3)") ) // &
-      & "* " // TRIM(STR( n=obj%sn_2, fm="(f0.3)")) //&
-      & "* J_" // TRIM( STR( n_2 ) ) // &
-      & "( " //  obj%varname%chars() // " )"
-  END IF
-END PROCEDURE J_GetStringToDisplay
+MODULE PROCEDURE Orthopol_GetStringForUID
+  ans = "P" // TRIM( STR( obj%GetOrder() ) ) // obj%varname%chars()
+END PROCEDURE Orthopol_GetStringForUID
 
 !----------------------------------------------------------------------------
 !                                                                  Weight
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_Weight
+MODULE PROCEDURE Orthopol_Weight
   ans = ( 1.0_DFP - x ) ** obj%alpha + (1.0_DFP + x ) ** obj%beta
-END PROCEDURE J_Weight
+END PROCEDURE Orthopol_Weight
 
 !----------------------------------------------------------------------------
 !                                                        GetRecurrenceCoeff
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_GetRecurrenceCoeff
+MODULE PROCEDURE Orthopol_GetRecurrenceCoeff
   !!
   CALL GetJacobiRecurrenceCoeff( &
-    & n=obj%n, &
+    & n=n, &
     & alpha=obj%alpha, &
     & beta=obj%beta, &
-    & alphaCoeff=coeff(0:,1), &
-    & betaCoeff=coeff(0:,2) )
+    & alphaCoeff=ans(0:,1), &
+    & betaCoeff=ans(0:,2) )
   !!
-END PROCEDURE J_GetRecurrenceCoeff
+END PROCEDURE Orthopol_GetRecurrenceCoeff
+
+!----------------------------------------------------------------------------
+!                                                  GetCoeffScale@GetMethods
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE Orthopol_GetCoeffScale
+  !!
+  REAL( DFP ) :: alpha, beta, coeff0( 0:n, 1:2 ), r1, r2, r3, r4
+  LOGICAL( LGT ) :: isMonic0, isOrthonormal0
+  INTEGER( I4B ) :: ii
+  !!
+  alpha = obj%alpha
+  beta = obj%beta
+  coeff0 = obj%GetRecurrenceCoeff( n = n+1 )
+  coeff = coeff0(0:n-1, 1:2)
+  !!
+  isMonic0 = INPUT( option=isMonic, default=.FALSE. )
+  isOrthonormal0 = INPUT( option=isOrthonormal, default=.FALSE. )
+  !!
+  IF( .NOT. isMonic0 ) THEN
+    !!
+    r1 = 2.0_DFP+alpha+beta
+    r2 = 1.0_DFP
+    r3 = 2.0_DFP
+    scale( 0, 1 ) = r1*r2/r3
+    !!
+    DO ii = 1, n-1
+      !!
+      r1 = 2.0_DFP*ii+alpha+beta + 2.0_DFP
+      r2 = r1 - 1.0_DFP
+      r3 = 2.0_DFP * (ii+1.0) * (ii+alpha+beta+1.0_DFP)
+      scale( ii, 1 ) = r1*r2/r3
+      !!
+    END DO
+    !!
+    IF( isOrthonormal0 ) THEN
+      !!
+      IF( n .GT. 1 ) THEN
+        !!
+        r1 = alpha+beta+3.0_DFP
+        r2 = 1.0_DFP
+        r3 = (1.0 + alpha)*(1.0 + beta)
+        scale( 1, 1 ) = scale( 1, 1 ) * SQRT(r1*r2/r3)
+        !!
+      END IF
+      !!
+      DO ii = 2, n-1
+        !!
+        r1 = 2.0_DFP*ii+alpha+beta + 1.0_DFP
+        r2 = ii * (ii+alpha+beta)
+        r3 = (r1-2.0_DFP)*(ii+beta)*(ii+alpha)
+        r4 = r1*r2/r3
+        scale( ii, 1 ) = scale( ii, 1 ) * SQRT(r4)
+        !!
+      END DO
+      !!
+    END IF
+    !!
+    scale( 0, 2 ) = scale( 0, 1 )
+    !!
+    DO ii = 1, n-1
+      scale( ii, 2 ) = scale( ii, 1 ) * scale( ii-1, 1)
+    END DO
+    !!
+  ELSE
+    !!
+    scale = 1.0_DFP
+    !!
+    IF( isOrthonormal0 ) THEN
+      scale( 0:n-1, 1 ) = 1.0_DFP / SQRT( coeff0( 1:n, 2 ) )
+      scale( 0:n-1, 2 ) = 1.0_DFP / SQRT( &
+        & coeff0( 1:n, 2 )*coeff0( 0:n-1, 2 ) )
+      !!
+    END IF
+    !!
+  END IF
+  !!
+END PROCEDURE Orthopol_GetCoeffScale
 
 !----------------------------------------------------------------------------
 !                                                                   Zeros
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_Zeros
-  ans = JacobiZeros( n = obj%n, alpha=obj%alpha, beta=obj%beta )
-END PROCEDURE J_Zeros
+MODULE PROCEDURE Orthopol_Zeros
+  ans = JacobiZeros( n = obj%GetOrder(), alpha=obj%alpha, beta=obj%beta )
+END PROCEDURE Orthopol_Zeros
 
 !----------------------------------------------------------------------------
 !                                                           GaussQuadrature
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_GaussQuadrature
-  CALL Reallocate( ans, obj%n, 2_I4B )
+MODULE PROCEDURE Orthopol_GaussQuadrature
+  CALL Reallocate( ans, obj%GetOrder(), 2_I4B )
   !!
   CALL JacobiGaussQuadrature( &
-    & n=obj%n, &
+    & n=obj%GetOrder(), &
     & alpha=obj%alpha, &
     & beta=obj%beta, &
     & pt=ans(:,1), &
     & wt=ans(:,2) )
   !!
-END PROCEDURE J_GaussQuadrature
+END PROCEDURE Orthopol_GaussQuadrature
 
 !----------------------------------------------------------------------------
 !                                                     GaussRadauQuadrature
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_GaussRadauQuadrature
-  CALL Reallocate( ans, obj%n+1, 2_I4B )
+MODULE PROCEDURE Orthopol_GaussRadauQuadrature
+  CALL Reallocate( ans, obj%GetOrder()+1, 2_I4B )
   !!
   CALL JacobiGaussRadauQuadrature( &
     & a = a, &
-    & n=obj%n, &
+    & n=obj%GetOrder(), &
     & alpha=obj%alpha, &
     & beta=obj%beta, &
     & pt=ans(:,1), &
     & wt=ans(:,2) )
   !!
-END PROCEDURE J_GaussRadauQuadrature
+END PROCEDURE Orthopol_GaussRadauQuadrature
 
 !----------------------------------------------------------------------------
 !                                                     GaussLobattoQuadrature
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE J_GaussLobattoQuadrature
-  CALL Reallocate( ans, obj%n+2, 2_I4B )
+MODULE PROCEDURE Orthopol_GaussLobattoQuadrature
+  CALL Reallocate( ans, obj%GetOrder()+2, 2_I4B )
   !!
   CALL JacobiGaussLobattoQuadrature( &
-    & n=obj%n, &
+    & n=obj%GetOrder(), &
     & alpha=obj%alpha, &
     & beta=obj%beta, &
     & pt=ans(:,1), &
     & wt=ans(:,2) )
   !!
-END PROCEDURE J_GaussLobattoQuadrature
+END PROCEDURE Orthopol_GaussLobattoQuadrature
 
 !----------------------------------------------------------------------------
 !
