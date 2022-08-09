ef8e785a965fa58813757bd466d260a5951aa729
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-09 10:54:03 +0900

    some renaming related

---
+++ src/submodules/AbstractOrthopol1D/src/AbstractOrthopolSpace1D_Class@ConstructorMethods.F90
@@ -0,0 +1,35 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(AbstractOrthopolSpace1D_Class) ConstructorMethods
+USE BaseMethod
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                Deallocate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE Orthopol_Deallocate
+  obj%n=0
+  obj%isMonic=.FALSE.
+  obj%isOrthonormal=.FALSE.
+  IF( ALLOCATED( obj%coeff ) ) DEALLOCATE( obj%coeff )
+  IF( ALLOCATED( obj%scale ) ) DEALLOCATE( obj%scale )
+END PROCEDURE Orthopol_Deallocate
+
+END SUBMODULE ConstructorMethods
