49097de80b8df9479490440943c1445d457a8d1a
Vikas Sharma <Vickysharma0812@gmail.com>
2022-08-19 00:57:56 +0900

    monomial3d

--- src/modules/Monomial3D/src/Monomial3D_Class.F90
+++ src/modules/Monomial3D/src/Monomial3D_Class.F90
@@ -24,33 +24,38 @@ USE Monomial1D_Class
 IMPLICIT NONE
 PRIVATE
 CHARACTER(LEN=*), PARAMETER :: modName = "Monomial3D_Class"
-INTEGER( I4B ), PARAMETER :: MAX_COMPONENTS = 3
 
 !----------------------------------------------------------------------------
 !                                                                Monomial3D_
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Monomial3D class is defined
 
 TYPE, EXTENDS( AbstractBasis3D_ ) :: Monomial3D_
   PRIVATE
-  TYPE( Monomial1D_ ) :: x( MAX_COMPONENTS )
+  INTEGER( I4B ) :: n1 = -1_I4B
+  INTEGER( I4B ) :: n2 = -1_I4B
+  INTEGER( I4B ) :: n3 = -1_I4B
   CONTAINS
     !!
     !! @ConstructorMethods
     !!
+    PROCEDURE, PASS( obj ) :: Initiate1 => func_Initiate1
+    PROCEDURE, PASS( obj ) :: Initiate2 => func_Initiate2
+    GENERIC, PUBLIC :: Initiate => Initiate1, Initiate2
     PROCEDURE, PUBLIC, PASS( obj ) :: Deallocate => func_Deallocate
     FINAL :: func_Final
     !!
     !! @GetMethods
     !!
-    PROCEDURE, PUBLIC, PASS( obj ) :: Eval=>func_Eval
+    PROCEDURE, PUBLIC, PASS( obj ) :: EvalScalar=>func_Eval
     PROCEDURE, PUBLIC, PASS( obj ) :: EvalGradient=>func_EvalGradient
     PROCEDURE, PUBLIC, PASS( obj ) :: Grad => func_Grad
     GENERIC, PUBLIC :: OPERATOR( .Grad. ) => Grad
-    PROCEDURE, PUBLIC, PASS( obj ) :: GetStringForUID => func_GetStringForUID
+    PROCEDURE, PUBLIC, PASS( obj ) :: GetStringForUID => &
+      & func_GetStringForUID
     PROCEDURE, PUBLIC, PASS( obj ) :: GetDisplayString =>  &
       & func_GetDisplayString
     PROCEDURE, PUBLIC, PASS( obj ) :: GetDegree => func_GetDegree
@@ -72,6 +77,9 @@ TYPE, EXTENDS( AbstractBasis3D_ ) :: Monomial3D_
     !!
     PROCEDURE, PUBLIC, PASS( obj ) :: AssignObjObj => func_AssignObjObj
     GENERIC, PUBLIC :: ASSIGNMENT( = ) => AssignObjObj
+    !!
+    !! @BasisMethods
+    !!
 END TYPE Monomial3D_
 
 PUBLIC :: Monomial3D_
@@ -87,19 +95,80 @@ END TYPE Monomial3DPointer_
 PUBLIC :: Monomial3DPointer_
 
 !----------------------------------------------------------------------------
+!                                                Initiate@ConstructorMethods
+!----------------------------------------------------------------------------
+
+!> authors: Vikas Sharma, Ph. D.
+! date: 18 Aug 2022
+! summary: Construct function for Monomial3D
+
+INTERFACE
+MODULE ELEMENTAL SUBROUTINE func_Initiate1( obj, n1, n2, n3, &
+  & name1, name2, name3 )
+  CLASS( Monomial3D_ ), INTENT( INOUT )  :: obj
+    !! Monomial3D = $x^{n1} y^{n2}$
+  INTEGER( I4B ), INTENT( IN ) :: n1
+    !! power of variable 1
+  INTEGER( I4B ), INTENT( IN ) :: n2
+    !! power for variable 2
+  INTEGER( I4B ), INTENT( IN ) :: n3
+    !! power for variable 3
+  CHARACTER( LEN = * ), INTENT( IN ) :: name1
+    !! name of variable 1
+  CHARACTER( LEN = * ), INTENT( IN ) :: name2
+    !! name of variable 2
+  CHARACTER( LEN = * ), INTENT( IN ) :: name3
+    !! name of variable 3
+END SUBROUTINE func_Initiate1
+END INTERFACE
+
+!----------------------------------------------------------------------------
+!                                                Initiate@ConstructorMethods
+!----------------------------------------------------------------------------
+
+!> authors: Vikas Sharma, Ph. D.
+! date: 18 Aug 2022
+! summary: Constructor function the Monomial3D
+
+INTERFACE
+MODULE ELEMENTAL SUBROUTINE func_Initiate2( obj, f1, f2, f3 )
+  CLASS( Monomial3D_ ), INTENT( INOUT ) :: obj
+    !! ans = f1*f2
+  CLASS( Monomial1D_ ), INTENT( IN ) :: f1
+    !! monomial for first variable
+  CLASS( Monomial1D_ ), INTENT( IN ) :: f2
+    !! monomial for second variable
+  CLASS( Monomial1D_ ), INTENT( IN ) :: f3
+    !! monomial for third variable
+END SUBROUTINE func_Initiate2
+END INTERFACE
+
+!----------------------------------------------------------------------------
 !                                             Monomial3D@ConstructorMethods
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Construct the Monomial3D
+! date: 18 Aug 2022
+! summary: Construct function for Monomial3D
 
 INTERFACE
-MODULE ELEMENTAL FUNCTION func_Monomial3D1( n1, n2, n3, name1, name2,  &
-  & name3 ) RESULT( ans )
-  INTEGER( I4B ), INTENT( IN ) :: n1, n2, n3
-  CHARACTER( LEN = * ), INTENT( IN ) :: name1, name2, name3
+MODULE ELEMENTAL FUNCTION func_Monomial3D1( n1, n2, n3, &
+  & name1, name2, name3 ) &
+  & RESULT( ans )
+  INTEGER( I4B ), INTENT( IN ) :: n1
+    !! power of variable 1
+  INTEGER( I4B ), INTENT( IN ) :: n2
+    !! power for variable 2
+  INTEGER( I4B ), INTENT( IN ) :: n3
+    !! power for variable 3
+  CHARACTER( LEN = * ), INTENT( IN ) :: name1
+    !! name of variable 1
+  CHARACTER( LEN = * ), INTENT( IN ) :: name2
+    !! name of variable 2
+  CHARACTER( LEN = * ), INTENT( IN ) :: name3
+    !! name of variable 3
   TYPE( Monomial3D_ ) :: ans
+    !! Monomial3D = $x^{n1} y^{n2} z^{n3}$
 END FUNCTION func_Monomial3D1
 END INTERFACE
 
@@ -114,15 +183,19 @@ PUBLIC :: Monomial3D
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Construct the Monomial3D
+! date: 18 Aug 2022
+! summary: Constructor function the Monomial3D
 
 INTERFACE
 MODULE ELEMENTAL FUNCTION func_Monomial3D2( f1, f2, f3 ) RESULT( ans )
   CLASS( Monomial1D_ ), INTENT( IN ) :: f1
+    !! monomial for first variable
   CLASS( Monomial1D_ ), INTENT( IN ) :: f2
+    !! monomial for second variable
   CLASS( Monomial1D_ ), INTENT( IN ) :: f3
+    !! monomial for third variable
   TYPE( Monomial3D_ ) :: ans
+    !! ans = f1*f2*f3
 END FUNCTION func_Monomial3D2
 END INTERFACE
 
@@ -135,15 +208,27 @@ END INTERFACE Monomial3D
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Construct the Monomial3D
+! date: 18 Aug 2022
+! summary: Construct function to return a pointer to the Monomial3D
 
 INTERFACE
-MODULE FUNCTION func_Monomial3D_Pointer1( n1, n2, n3, name1, name2, name3 ) &
+MODULE FUNCTION func_Monomial3D_Pointer1( n1, n2, n3, &
+  & name1, name2, name3 ) &
   & RESULT( ans )
-  INTEGER( I4B ), INTENT( IN ) :: n1, n2, n3
-  CHARACTER( LEN = * ), INTENT( IN ) :: name1, name2, name3
+  INTEGER( I4B ), INTENT( IN ) :: n1
+    !! power of variable 1
+  INTEGER( I4B ), INTENT( IN ) :: n2
+    !! power of variable 2
+  INTEGER( I4B ), INTENT( IN ) :: n3
+    !! power of variable 3
+  CHARACTER( LEN = * ), INTENT( IN ) :: name1
+    !! name of variable 1
+  CHARACTER( LEN = * ), INTENT( IN ) :: name2
+    !! name of variable 2
+  CHARACTER( LEN = * ), INTENT( IN ) :: name3
+    !! name of variable 3
   CLASS( Monomial3D_ ), POINTER :: ans
+    !! returned 2D monomial
 END FUNCTION func_Monomial3D_Pointer1
 END INTERFACE
 
@@ -158,8 +243,8 @@ PUBLIC :: Monomial3D_Pointer
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Construct the Monomial3D
+! date: 18 Aug 2022
+! summary: Construct function to return a pointer to the Monomial3D
 
 INTERFACE
 MODULE FUNCTION func_Monomial3D_Pointer2( f1, f2, f3 ) RESULT( ans )
@@ -179,7 +264,7 @@ END INTERFACE Monomial3D_Pointer
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Deallocate the object
 
 INTERFACE
@@ -205,15 +290,13 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Evaluate the function
 
 INTERFACE
   MODULE ELEMENTAL FUNCTION func_eval( obj, x, y, z ) RESULT( ans )
     CLASS( Monomial3D_ ), INTENT( IN ) :: obj
-    REAL( DFP ), INTENT( IN ) :: x
-    REAL( DFP ), INTENT( IN ) :: y
-    REAL( DFP ), INTENT( IN ) :: z
+    REAL( DFP ), INTENT( IN ) :: x, y, z
     REAL( DFP ) :: ans
   END FUNCTION func_eval
 END INTERFACE
@@ -223,19 +306,19 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Evaluate the function
+! date: 18 Aug 2022
+! summary: Evaluate the gradient
 
 INTERFACE
-  MODULE ELEMENTAL FUNCTION func_EvalGradient( obj, x, y, z, dim ) &
-    & RESULT( ans )
-    CLASS( Monomial3D_ ), INTENT( IN ) :: obj
-    REAL( DFP ), INTENT( IN ) :: x
-    REAL( DFP ), INTENT( IN ) :: y
-    REAL( DFP ), INTENT( IN ) :: z
-    INTEGER( I4B ), INTENT( IN ) :: dim
-    REAL( DFP ) :: ans
-  END FUNCTION func_EvalGradient
+MODULE ELEMENTAL FUNCTION func_EvalGradient( obj, x, y, z, dim ) &
+  & RESULT( ans )
+  CLASS( Monomial3D_ ), INTENT( IN ) :: obj
+  REAL( DFP ), INTENT( IN ) :: x
+  REAL( DFP ), INTENT( IN ) :: y
+  REAL( DFP ), INTENT( IN ) :: z
+  INTEGER( I4B ), INTENT( IN ) :: dim
+  REAL( DFP ) :: ans
+END FUNCTION func_EvalGradient
 END INTERFACE
 
 !----------------------------------------------------------------------------
@@ -243,15 +326,15 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Evaluate the function
+! date: 18 Aug 2022
+! summary: Return the gradient of a monomial.
 
 INTERFACE
-  MODULE ELEMENTAL FUNCTION func_Grad( obj, dim ) RESULT( ans )
-    CLASS( Monomial3D_ ), INTENT( IN ) :: obj
-    INTEGER( I4B ), INTENT( IN ) :: dim
-    TYPE( Monomial3D_ ) :: ans
-  END FUNCTION func_Grad
+MODULE ELEMENTAL FUNCTION func_Grad( obj, dim ) RESULT( ans )
+  CLASS( Monomial3D_ ), INTENT( IN ) :: obj
+  INTEGER( I4B ), INTENT( IN ) :: dim
+  TYPE( Monomial3D_ ) :: ans
+END FUNCTION func_Grad
 END INTERFACE
 
 !----------------------------------------------------------------------------
@@ -259,8 +342,8 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Evaluate the gradient of function df/dx
+! date: 18 Aug 2022
+! summary: Return a string for generating the UID of monomial
 
 INTERFACE
   MODULE ELEMENTAL FUNCTION func_GetStringForUID( obj ) RESULT( ans )
@@ -274,7 +357,7 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Get the display string
 
 INTERFACE
@@ -289,13 +372,13 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
-! summary: Evaluate the gradient of function df/dx
+! date: 18 Aug 2022
+! summary: Get the degree $(n1,n2,n3)$ of the monomial.
 
 INTERFACE
-  MODULE ELEMENTAL FUNCTION func_GetDegree( obj ) RESULT( ans )
+  MODULE PURE FUNCTION func_GetDegree( obj ) RESULT( ans )
     CLASS( Monomial3D_ ), INTENT( IN ) :: obj
-    INTEGER( I4B ) :: ans
+    INTEGER( I4B ) :: ans(3)
   END FUNCTION func_GetDegree
 END INTERFACE
 
@@ -304,7 +387,7 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Get the coefficient
 
 INTERFACE
@@ -319,7 +402,7 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Display the content of Monomial3D
 
 INTERFACE
@@ -335,7 +418,7 @@ END INTERFACE
 !----------------------------------------------------------------------------
 
 !> authors: Vikas Sharma, Ph. D.
-! date: 14 May 2022
+! date: 18 Aug 2022
 ! summary: Multiplication two Monomial1D
 
 INTERFACE
@@ -351,11 +434,73 @@ END INTERFACE
 !
 !----------------------------------------------------------------------------
 
+!> author: Vikas Sharma, Ph. D.
+! date: 18 Aug 2022
+! summary: 	Assignment operator overloaded
+
 INTERFACE
-MODULE ELEMENTAL SUBROUTINE func_AssignObjObj( obj, obj2 )
+MODULE PURE SUBROUTINE func_AssignObjObj( obj, obj2 )
   CLASS( Monomial3D_ ), INTENT( INOUT ) :: obj
   CLASS( Monomial3D_ ), INTENT( IN ) :: obj2
 END SUBROUTINE func_AssignObjObj
 END INTERFACE
+
+!----------------------------------------------------------------------------
+!                                                      Assign@AssignMethods
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 18 Aug 2022
+! summary: 	Assignment operator overloaded
+
+INTERFACE
+MODULE PURE SUBROUTINE func_AssignObjVecObjVec( obj, obj2 )
+  CLASS( Monomial3D_ ), ALLOCATABLE, INTENT( INOUT ) :: obj( : )
+  CLASS( Monomial3D_ ), INTENT( IN ) :: obj2( : )
+END SUBROUTINE func_AssignObjVecObjVec
+END INTERFACE
+
+INTERFACE ASSIGNMENT(=)
+  MODULE PROCEDURE func_AssignObjVecObjVec
+END INTERFACE ASSIGNMENT(=)
+
+PUBLIC :: ASSIGNMENT(=)
+
+!----------------------------------------------------------------------------
+!                                                   Monomials3D@BasisMethods
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 13 Aug 2022
+! summary: Returns monomial basis for lagrange polynomials
+
+INTERFACE
+MODULE FUNCTION func_Monomials3D( order, name1, name2, name3, elemType ) &
+  & RESULT( ans )
+  INTEGER( I4B ), INTENT( IN ) :: order
+    !! order
+  CHARACTER( LEN = * ), INTENT( IN ) :: name1
+    !! "x"
+  CHARACTER( LEN = * ), INTENT( IN ) :: name2
+    !! "y"
+  CHARACTER( LEN = * ), INTENT( IN ) :: name3
+    !! "z"
+  INTEGER( I4B ), INTENT( IN ) :: elemType
+    !! element type
+    !! tetrahedron, hexahedron, pyramid, prism
+  TYPE( Monomial3D_ ), ALLOCATABLE :: ans( : )
+    !! Monomials in 3D
+END FUNCTION func_Monomials3D
+END INTERFACE
+
+INTERFACE Monomials3D
+  MODULE PROCEDURE func_Monomials3D
+END INTERFACE Monomials3D
+
+PUBLIC :: Monomials3D
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END MODULE Monomial3D_Class
