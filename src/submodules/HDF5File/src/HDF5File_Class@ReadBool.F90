2f175350454a68982fe69e06bf9d8853f9457911
Vikas Sharma <vickysharma0812@gmail.com>
2023-09-25 14:31:55 +0900

    Update: HDF5File_Class
    Formatting: names of some methods.

--- src/submodules/HDF5File/src/HDF5File_Class@ReadBool.F90
+++ src/submodules/HDF5File/src/HDF5File_Class@ReadBool.F90
@@ -25,27 +25,27 @@ CONTAINS
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE hdf5_read_b0
-  CHARACTER( LEN=1 ) :: valsc
-  CHARACTER( LEN=LEN(dsetname)+1 ) :: path
-  INTEGER( HSIZE_T ), DIMENSION( 1 ) :: dims
-  INTEGER( I4B ), PARAMETER :: rank=0
-  INTEGER( I4B ) :: error
-  INTEGER(HID_T) :: mem, dspace_id,dset_id
+CHARACTER(1) :: valsc
+CHARACTER(LEN(dsetname) + 1) :: path
+INTEGER(HSIZE_T), DIMENSION(1) :: dims
+INTEGER(I4B), PARAMETER :: rank = 0
+INTEGER(I4B) :: error
+INTEGER(HID_T) :: mem, dspace_id, dset_id
 
-  path=dsetname
-  ! Read the dataset
-  mem=H5T_NATIVE_CHARACTER
-  CALL preRead(obj,path,rank,dset_id,dspace_id,dims,error)
-  IF(error >= 0) THEN
-    CALL h5dread_f(dset_id,mem,valsc,dims,error)
+path = dsetname
+! Read the dataset
+mem = H5T_NATIVE_CHARACTER
+CALL preRead(obj, path, rank, dset_id, dspace_id, dims, error)
+IF (error >= 0) THEN
+  CALL h5dread_f(dset_id, mem, valsc, dims, error)
   ! Convert to logical from character
-    IF(valsc=='F') THEN
-      vals=.FALSE.
-    ELSE
-      vals=.TRUE.
-    ENDIF
-  ENDIF
-  CALL postRead(obj,path,dset_id,dspace_id,error)
+  IF (valsc == 'F') THEN
+    vals = .FALSE.
+  ELSE
+    vals = .TRUE.
+  END IF
+END IF
+CALL postRead(obj, path, dset_id, dspace_id, error)
 END PROCEDURE hdf5_read_b0
 
 !----------------------------------------------------------------------------
@@ -53,35 +53,35 @@ END PROCEDURE hdf5_read_b0
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE hdf5_read_b1
-  CHARACTER, ALLOCATABLE :: valsc(:)
-  CHARACTER( LEN=LEN(dsetname)+1 ) :: path
-  INTEGER( I4B ) :: i, error
-  INTEGER( HSIZE_T ), DIMENSION( 1 ) :: dims
-  INTEGER( I4B ), PARAMETER :: rank=1
-  INTEGER( HID_T ) :: mem, dspace_id,dset_id
+CHARACTER, ALLOCATABLE :: valsc(:)
+CHARACTER(LEN(dsetname) + 1) :: path
+INTEGER(I4B) :: i, error
+INTEGER(HSIZE_T), DIMENSION(1) :: dims
+INTEGER(I4B), PARAMETER :: rank = 1
+INTEGER(HID_T) :: mem, dspace_id, dset_id
 
-  path=dsetname
-  ! Allocate space in data if needed, make sure it is the right size
-  CALL preRead(obj,path,rank,dset_id,dspace_id,dims,error)
-  IF(error >= 0) THEN
-    IF(ALLOCATED(vals)) THEN
-      IF(ANY(SHAPE(vals) /= dims)) THEN
-        DEALLOCATE(vals)
-        ALLOCATE(vals(dims(1)))
-      ENDIF
-    ELSE
-      ALLOCATE(vals(dims(1)))
-    ENDIF
-    ALLOCATE(valsc(dims(1)))
-    mem=H5T_NATIVE_CHARACTER
-    CALL h5dread_f(dset_id,mem,valsc,dims,error)
-    vals=.FALSE.
-    FORALL(i=1:SIZE(vals),valsc(i) == 'T')
-      vals(i)=.TRUE.
-    ENDFORALL
-    DEALLOCATE(valsc)
-  ENDIF
-  CALL postRead(obj,path,dset_id,dspace_id,error)
+path = dsetname
+! Allocate space in data if needed, make sure it is the right size
+CALL preRead(obj, path, rank, dset_id, dspace_id, dims, error)
+IF (error >= 0) THEN
+  IF (ALLOCATED(vals)) THEN
+    IF (ANY(SHAPE(vals) /= dims)) THEN
+      DEALLOCATE (vals)
+      ALLOCATE (vals(dims(1)))
+    END IF
+  ELSE
+    ALLOCATE (vals(dims(1)))
+  END IF
+  ALLOCATE (valsc(dims(1)))
+  mem = H5T_NATIVE_CHARACTER
+  CALL h5dread_f(dset_id, mem, valsc, dims, error)
+  vals = .FALSE.
+  DO CONCURRENT(i=1:SIZE(vals), valsc(i) == 'T')
+    vals(i) = .TRUE.
+  END DO
+  DEALLOCATE (valsc)
+END IF
+CALL postRead(obj, path, dset_id, dspace_id, error)
 END PROCEDURE hdf5_read_b1
 
 !----------------------------------------------------------------------------
@@ -89,38 +89,38 @@ END PROCEDURE hdf5_read_b1
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE hdf5_read_b2
-  CHARACTER( LEN=1 ), ALLOCATABLE :: valsc(:,:)
-  CHARACTER( LEN=LEN(dsetname)+1 ) :: path
-  INTEGER( I4B ) :: i,j,error
-  INTEGER( HSIZE_T ), DIMENSION( 2 ) :: dims
-  INTEGER( I4B ), PARAMETER :: rank=2
-  INTEGER( HID_T ) :: mem, dspace_id,dset_id
+CHARACTER(1), ALLOCATABLE :: valsc(:, :)
+CHARACTER(LEN(dsetname) + 1) :: path
+INTEGER(I4B) :: i, j, error
+INTEGER(HSIZE_T), DIMENSION(2) :: dims
+INTEGER(I4B), PARAMETER :: rank = 2
+INTEGER(HID_T) :: mem, dspace_id, dset_id
 
-  path=dsetname
-  CALL preRead(obj,path,rank,dset_id,dspace_id,dims,error)
-  IF(error >= 0) THEN
-    IF(ALLOCATED(vals)) THEN
-      IF(ANY(SHAPE(vals) /= dims)) THEN
-        DEALLOCATE(vals)
-        ALLOCATE(vals(dims(1),dims(2)))
-      ENDIF
-    ELSE
-      ALLOCATE(vals(dims(1),dims(2)))
-    ENDIF
-    ALLOCATE(valsc(dims(1),dims(2)))
+path = dsetname
+CALL preRead(obj, path, rank, dset_id, dspace_id, dims, error)
+IF (error >= 0) THEN
+  IF (ALLOCATED(vals)) THEN
+    IF (ANY(SHAPE(vals) /= dims)) THEN
+      DEALLOCATE (vals)
+      ALLOCATE (vals(dims(1), dims(2)))
+    END IF
+  ELSE
+    ALLOCATE (vals(dims(1), dims(2)))
+  END IF
+  ALLOCATE (valsc(dims(1), dims(2)))
 
-    ! Read the dataset
-    mem=H5T_NATIVE_CHARACTER
-    CALL h5dread_f(dset_id,mem,valsc,dims,error)
-    ! Convert from surrogate character array to boolean array
-    vals=.FALSE.
-    FORALL(i=1:SIZE(vals,DIM=1),j=1:SIZE(vals,DIM=2),valsc(i,j) == 'T')
-      vals(i,j)=.TRUE.
-    ENDFORALL
+  ! Read the dataset
+  mem = H5T_NATIVE_CHARACTER
+  CALL h5dread_f(dset_id, mem, valsc, dims, error)
+  ! Convert from surrogate character array to boolean array
+  vals = .FALSE.
+  DO concurrent(i=1:SIZE(vals, DIM=1), j=1:SIZE(vals, DIM=2), valsc(i, j) == 'T')
+    vals(i, j) = .TRUE.
+  END DO
 
-    DEALLOCATE(valsc)
-  ENDIF
-  CALL postRead(obj,path,dset_id,dspace_id,error)
+  DEALLOCATE (valsc)
+END IF
+CALL postRead(obj, path, dset_id, dspace_id, error)
 END PROCEDURE hdf5_read_b2
 
 !----------------------------------------------------------------------------
