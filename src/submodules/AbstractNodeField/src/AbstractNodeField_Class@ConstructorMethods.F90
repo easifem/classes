72275c8dac5e3150d422e90fae1fba6f316e586b
Vikas Sharma <vickysharma0812@gmail.com>
2023-11-25 18:20:52 +0900

    Update AbstractNodeField_Class@ConstructorMethods.F90
    Minor formatting

--- src/submodules/AbstractNodeField/src/AbstractNodeField_Class@ConstructorMethods.F90
+++ src/submodules/AbstractNodeField/src/AbstractNodeField_Class@ConstructorMethods.F90
@@ -21,63 +21,11 @@ IMPLICIT NONE
 CONTAINS
 
 !----------------------------------------------------------------------------
-!                                                                 Initiate2
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE anf_Initiate2
-CHARACTER(*), PARAMETER :: myName = "anf_Initiate2"
-INTEGER(I4B) :: ii, tsize
-
-CALL obj%DEALLOCATE()
-CALL AbstractFieldInitiate2( &
-  & obj=obj, &
-  & obj2=obj2, &
-  & copyFull=copyFull, &
-  & copyStructure=copyStructure, &
-  & usePointer=usePointer)
-
-SELECT TYPE (obj2); CLASS IS (AbstractNodeField_)
-  obj%tSize = obj2%tSize
-  obj%realVec = obj2%realVec
-  obj%dof = obj2%dof
-END SELECT
-
-END PROCEDURE anf_Initiate2
-
-!----------------------------------------------------------------------------
-!                                                            anf_Initiate3
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE anf_Initiate3
-CHARACTER(*), PARAMETER :: myName = "anf_Initiate3"
-CALL e%raiseError(modName//'::'//myName//" - "// &
-  & '[IMPLEMENTATION ERROR] :: Initiate3 should be implemented by the'// &
-  & ' child of AbstractNodeField_')
-END PROCEDURE anf_Initiate3
-
-!----------------------------------------------------------------------------
-!                                                            Deallocate
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE anf_Deallocate
-CALL AbstractFieldDeallocate(obj)
-obj%dof_tPhysicalVars = 0
-obj%dof_storageFMT = NODES_FMT
-IF (ALLOCATED(obj%dof_spaceCompo)) DEALLOCATE (obj%dof_spaceCompo)
-IF (ALLOCATED(obj%dof_timeCompo)) DEALLOCATE (obj%dof_timeCompo)
-IF (ALLOCATED(obj%dof_tNodes)) DEALLOCATE (obj%dof_tNodes)
-IF (ALLOCATED(obj%dof_names_char)) DEALLOCATE (obj%dof_names_char)
-obj%tSize = 0
-CALL DEALLOCATE (obj%realVec)
-CALL DEALLOCATE (obj%dof)
-END PROCEDURE anf_Deallocate
-
-!----------------------------------------------------------------------------
-!                                                             Initiate
+!                                                 AbstractNodeFieldInitiate
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE AbstractNodeFieldInitiate
-CHARACTER(*), PARAMETER :: myName = "AbstractNodeFieldInitiate"
+CHARACTER(*), PARAMETER :: myName = "AbstractNodeFieldInitiate()"
 
 CALL AbstractFieldInitiate(obj=obj, param=param, prefix=prefix, dom=dom)
 
@@ -131,6 +79,58 @@ IF (obj%global_n .EQ. 0) THEN
 END IF
 
 END PROCEDURE AbstractNodeFieldInitiate
+
+!----------------------------------------------------------------------------
+!                                                                 Initiate2
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE anf_Initiate2
+CHARACTER(*), PARAMETER :: myName = "anf_Initiate2"
+INTEGER(I4B) :: ii, tsize
+
+CALL obj%DEALLOCATE()
+CALL AbstractFieldInitiate2( &
+  & obj=obj, &
+  & obj2=obj2, &
+  & copyFull=copyFull, &
+  & copyStructure=copyStructure, &
+  & usePointer=usePointer)
+
+SELECT TYPE (obj2); CLASS IS (AbstractNodeField_)
+  obj%tSize = obj2%tSize
+  obj%realVec = obj2%realVec
+  obj%dof = obj2%dof
+END SELECT
+
+END PROCEDURE anf_Initiate2
+
+!----------------------------------------------------------------------------
+!                                                            anf_Initiate3
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE anf_Initiate3
+CHARACTER(*), PARAMETER :: myName = "anf_Initiate3"
+CALL e%raiseError(modName//'::'//myName//" - "// &
+  & '[IMPLEMENTATION ERROR] :: Initiate3 should be implemented by the'// &
+  & ' child of AbstractNodeField_')
+END PROCEDURE anf_Initiate3
+
+!----------------------------------------------------------------------------
+!                                                            Deallocate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE anf_Deallocate
+CALL AbstractFieldDeallocate(obj)
+obj%dof_tPhysicalVars = 0
+obj%dof_storageFMT = NODES_FMT
+IF (ALLOCATED(obj%dof_spaceCompo)) DEALLOCATE (obj%dof_spaceCompo)
+IF (ALLOCATED(obj%dof_timeCompo)) DEALLOCATE (obj%dof_timeCompo)
+IF (ALLOCATED(obj%dof_tNodes)) DEALLOCATE (obj%dof_tNodes)
+IF (ALLOCATED(obj%dof_names_char)) DEALLOCATE (obj%dof_names_char)
+obj%tSize = 0
+CALL DEALLOCATE (obj%realVec)
+CALL DEALLOCATE (obj%dof)
+END PROCEDURE anf_Deallocate
 
 !----------------------------------------------------------------------------
 !
