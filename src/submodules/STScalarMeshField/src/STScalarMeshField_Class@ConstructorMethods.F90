88d29e4b1bee222e509b4ce5fcbb3bfd4000f6f1
Vikas Sharma <vickysharma0812@gmail.com>
2023-12-06 10:58:37 +0900

    Updates in STScalarMeshField_Class
    - Working on feature #262
    - Construct from `UserFunction` and `AbstractMaterial` is now possible
    - Set values by using `UserFunction` is possible.

--- src/submodules/STScalarMeshField/src/STScalarMeshField_Class@ConstructorMethods.F90
+++ src/submodules/STScalarMeshField/src/STScalarMeshField_Class@ConstructorMethods.F90
@@ -32,6 +32,20 @@ ELSE
   s = [nns, nnt]; n = 2
 END IF
 
+IF (varType .EQ. Time) THEN
+  CALL SetAbstractMeshFieldParam( &
+    & param=param, &
+    & prefix="STScalarMeshField", &
+    & name=name, &
+    & fieldType=fieldType, &
+    & varType=varType, &
+    & engine=engine, &
+    & defineOn=defineOn, &
+    & rank=Scalar, &
+    & s=s(n:n))
+  RETURN
+END IF
+
 CALL SetAbstractMeshFieldParam( &
   & param=param, &
   & prefix="STScalarMeshField", &
@@ -42,7 +56,78 @@ CALL SetAbstractMeshFieldParam( &
   & defineOn=defineOn, &
   & rank=Scalar, &
   & s=s(1:n))
+
 END PROCEDURE SetSTScalarMeshFieldParam
+
+!----------------------------------------------------------------------------
+!                                                                   Initiate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_Initiate4
+CHARACTER(*), PARAMETER :: myName = "obj_Initiate4()"
+LOGICAL(LGT) :: isok
+INTEGER(I4B) :: returnType, argType, nns, varType, fieldType
+TYPE(ParameterList_) :: param
+CLASS(ReferenceElement_), POINTER :: refelem
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & '[START] ')
+#endif DEBUG_VER
+
+refelem => NULL()
+refelem => mesh%GetRefElemPointer()
+isok = ASSOCIATED(refelem)
+IF (.NOT. isok) THEN
+  CALL e%RaiseError(modName//'::'//myName//' - '// &
+    & '[INTERNAL ERROR] :: refelem pointer not found.')
+  RETURN
+END IF
+nns = (.NNE.refelem)
+
+returnType = func%GetReturnType()
+argType = func%GetArgType()
+
+isok = returnType .EQ. Scalar
+IF (.NOT. isok) THEN
+  CALL e%RaiseError(modName//'::'//myName//' - '// &
+    & '[INTERNAL ERROR] :: returnType should be Scalar.')
+  RETURN
+END IF
+
+fieldType = TypeField%normal
+varType = argType
+IF (argType .EQ. Constant) THEN
+  fieldType = TypeField%constant
+  varType = Constant
+END IF
+
+isok = PRESENT(nnt)
+IF (.NOT. isok) THEN
+  CALL e%RaiseError(modName//'::'//myName//' - '// &
+    & '[INTERNAL ERROR] :: NNT should be present when varType'//  &
+    & ' in userFunction is Time or SpaceTime.')
+  RETURN
+END IF
+
+CALL param%Initiate()
+
+CALL SetSTScalarMeshFieldParam(param=param, name=name,  &
+  & fieldType=fieldType, varType=varType, engine=engine, &
+  & defineOn=Nodal, nns=nns, nnt=nnt)
+
+CALL obj%Initiate(param=param, mesh=mesh)
+
+CALL param%DEALLOCATE()
+
+NULLIFY (refelem)
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & '[END] ')
+#endif DEBUG_VER
+
+END PROCEDURE obj_Initiate4
 
 !----------------------------------------------------------------------------
 !                                                             Deallocate
