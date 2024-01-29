a4ea790e4f690f4e37838c68c4bf480a75424378
Vikas@easifem@archlinux@work <vickysharma0812@gmail.com>
2024-01-29 17:24:13 +0900

    Update in Mesh_Class Import

--- src/submodules/Mesh/src/Mesh_Class@IOMethods.F90
+++ src/submodules/Mesh/src/Mesh_Class@IOMethods.F90
@@ -92,59 +92,59 @@ DO CONCURRENT(ii=1:obj%tElements)
 END DO
 
 isok = (obj%elemType .EQ. Point1) .OR. (obj%elemType .EQ. 0)
+
 IF (isok) THEN
   obj%tNodes = 1
-  IF (ALLOCATED(obj%nodeData)) DEALLOCATE (obj%nodeData)
   ALLOCATE (obj%nodeData(obj%tNodes))
-  obj%nodeData(1)%globalNodeNum = 1
   obj%nodeData(1)%localNodeNum = 1
-  obj%nodeData(1)%nodeType = BOUNDARY_NODE
-ELSE
-  obj%maxNptrs = MAXVAL(connectivity)
-  obj%minNptrs = MINVAL(connectivity)
-  CALL Reallocate(obj%local_Nptrs, obj%maxNptrs)
-
-  DO CONCURRENT(ii=1:obj%tElements)
-    obj%elementData(ii)%globalNodes = connectivity(:, ii)
-    obj%local_Nptrs(connectivity(:, ii)) = connectivity(:, ii)
-  END DO
-
-  obj%tNodes = COUNT(obj%local_Nptrs .NE. 0)
-  IF (ALLOCATED(obj%nodeData)) DEALLOCATE (obj%nodeData)
-  ALLOCATE (obj%nodeData(obj%tNodes))
-  dummy = 0
-
-  DO ii = 1, obj%maxNptrs
-    IF (obj%local_Nptrs(ii) .NE. 0) THEN
-      dummy = dummy + 1
-      obj%nodeData(dummy)%globalNodeNum = obj%local_Nptrs(ii)
-      obj%nodeData(dummy)%localNodeNum = dummy
-      obj%nodeData(dummy)%nodeType = BOUNDARY_NODE
-      ! The above step is unusual, but we know the position of
-      ! internal nptrs, so later we will set the
-      ! those nodes as INTERNAL_NODE, in this way we can
-      ! identify the boundary nodes
-      obj%local_Nptrs(ii) = dummy
-    END IF
-  END DO
-
-END IF
-
-!> reading internalNptrs, nodeData%globalNodeNumber,
-!> nodeData%localNodeNumber, nodeData%nodeType
-!> mark INTERNAL_NODE
-
-abool = obj%elemType .EQ. Point1 .OR. obj%elemType .EQ. 0
-IF (abool) THEN
   obj%nodeData(1)%globalNodeNum = internalNptrs(1)
   obj%nodeData(1)%nodeType = INTERNAL_NODE
-ELSE
-  DO ii = 1, SIZE(internalNptrs)
-    jj = obj%GetLocalNodeNumber(internalNptrs(ii))
-    obj%nodeData(jj)%globalNodeNum = internalNptrs(ii)
-    obj%nodeData(jj)%nodeType = INTERNAL_NODE
-  END DO
+
+  IF (ALLOCATED(elemNumber)) DEALLOCATE (elemNumber)
+  IF (ALLOCATED(connectivity)) DEALLOCATE (connectivity)
+  IF (ALLOCATED(internalNptrs)) DEALLOCATE (internalNptrs)
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+    & '[END] ')
+#endif
+
+  RETURN
 END IF
+
+obj%maxNptrs = MAXVAL(connectivity)
+obj%minNptrs = MINVAL(connectivity)
+CALL Reallocate(obj%local_Nptrs, obj%maxNptrs)
+
+DO CONCURRENT(ii=1:obj%tElements)
+  obj%elementData(ii)%globalNodes = connectivity(:, ii)
+  obj%local_Nptrs(connectivity(:, ii)) = connectivity(:, ii)
+END DO
+
+obj%tNodes = COUNT(obj%local_Nptrs .NE. 0)
+IF (ALLOCATED(obj%nodeData)) DEALLOCATE (obj%nodeData)
+ALLOCATE (obj%nodeData(obj%tNodes))
+dummy = 0
+
+DO ii = 1, obj%maxNptrs
+  IF (obj%local_Nptrs(ii) .NE. 0) THEN
+    dummy = dummy + 1
+    obj%nodeData(dummy)%globalNodeNum = obj%local_Nptrs(ii)
+    obj%nodeData(dummy)%localNodeNum = dummy
+    obj%nodeData(dummy)%nodeType = BOUNDARY_NODE
+    ! The above step is unusual, but we know the position of
+    ! internal nptrs, so later we will set the
+    ! those nodes as INTERNAL_NODE, in this way we can
+    ! identify the boundary nodes
+    obj%local_Nptrs(ii) = dummy
+  END IF
+END DO
+
+DO ii = 1, SIZE(internalNptrs)
+  jj = obj%GetLocalNodeNumber(internalNptrs(ii))
+  obj%nodeData(jj)%globalNodeNum = internalNptrs(ii)
+  obj%nodeData(jj)%nodeType = INTERNAL_NODE
+END DO
 
 isok = obj%tElements .GT. 0
 abool = obj%xidim .GT. 0
