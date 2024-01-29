8d5850868bd63975b8e21c43825e98f637326823
Vikas@easifem@archlinux@work <vickysharma0812@gmail.com>
2024-01-29 12:37:42 +0900

    Minor fix in Mesh_Class

--- src/modules/Mesh/src/Mesh_Class.F90
+++ src/modules/Mesh/src/Mesh_Class.F90
@@ -432,12 +432,12 @@ END INTERFACE
 !```
 
 INTERFACE MeshDisplay
-  MODULE SUBROUTINE obj_Display(obj, msg, UnitNo)
+  MODULE SUBROUTINE obj_Display(obj, msg, unitno)
     CLASS(Mesh_), INTENT(INOUT) :: obj
     !! mesh object
     CHARACTER(*), INTENT(IN) :: msg
     !! message on screen
-    INTEGER(I4B), OPTIONAL, INTENT(IN) :: UnitNo
+    INTEGER(I4B), OPTIONAL, INTENT(IN) :: unitno
     !! unit number of ouput file
   END SUBROUTINE obj_Display
 END INTERFACE MeshDisplay
