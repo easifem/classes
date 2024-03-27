68e92b8ab4c0d8f885f2ba679e631d883ee3f188
Vikas Sharma <vickysharma0812@gmail.com>
2024-03-27 20:54:10 +0900

    EASIFEM-92
    Updating InitiateFaceConnectivity in AbstractMesh_Class.F90

--- src/submodules/AbstractMesh/src/AbstractMesh_Class@FaceDataMethods.F90
+++ src/submodules/AbstractMesh/src/AbstractMesh_Class@FaceDataMethods.F90
@@ -17,9 +17,14 @@
 
 SUBMODULE(AbstractMesh_Class) FaceDataMethods
 USE ReferenceElement_Method, ONLY: REFELEM_MAX_FACES,  &
-  & REFELEM_MAX_POINTS, RefElemGetGeoParam
-USE ReferenceQuadrangle_Method, ONLY: HelpFaceData_Quadrangle,  &
-& FaceShapeMetaData_Quadrangle
+  & REFELEM_MAX_POINTS,  &
+  & RefElemGetGeoParam,  &
+  & IsQuadrangle
+
+USE ReferenceTriangle_Method, ONLY: FaceShapeMetaData_Triangle
+
+USE ReferenceQuadrangle_Method, ONLY: FaceShapeMetaData_Quadrangle
+
 USE ReallocateUtility, ONLY: Reallocate
 USE FaceData_Class
 USE FaceDataBinaryTree_Class
@@ -36,10 +41,10 @@ MODULE PROCEDURE obj_InitiateFaceConnectivity
 CHARACTER(*), PARAMETER :: myName = "obj_InitiateFaceConnectivity()"
 INTEGER(I4B) :: tElements, iel, elemType, tFaces,  &
   & localFaces(4_I4B, REFELEM_MAX_FACES), face(4), sorted_face(4),  &
-  & nptrs(REFELEM_MAX_POINTS), tNodes, tsize1, tsize2, iface,  &
+  & tNodes, tsize1, tsize2, iface,  &
   & faceElemType(REFELEM_MAX_FACES), tFaceNodes(REFELEM_MAX_FACES),  &
-  & aint, bint, faceOrient(3_I4B)
-LOGICAL(LGT) :: problem
+  & aint, faceOrient(3_I4B)
+LOGICAL(LGT) :: problem, abool
 TYPE(FaceDataBinaryTree_) :: faceTree
 TYPE(FaceData_) :: faceValue
 TYPE(FaceData_), POINTER :: facePtr
@@ -64,27 +69,32 @@ tElements = obj%GetTotalElements()
 CALL faceTree%Initiate()
 
 DO iel = 1, tElements
+
   problem = .NOT. obj%elementData(iel)%isActive
   IF (problem) CYCLE
   elemType = obj%elementData(iel)%name
   CALL RefElemGetGeoParam(elemType=elemType, &
     & tFaces=tFaces, tNodes=tNodes, faceCon=localFaces, &
     & faceOpt=1_I4B, faceElemType=faceElemType, &
-    & tFaceNodes=tFaceNodes)
+    & tFaceNodes=tFaceNodes, order=1_I4B)
 
   CALL Reallocate(obj%elementData(iel)%globalFaces, tFaces)
   CALL Reallocate(obj%elementData(iel)%faceOrient, 3_I4B, tFaces)
-
-  nptrs(1:tNodes) = obj%elementData(iel)%globalNodes
 
   DO iface = 1, tFaces
 
     aint = tFaceNodes(iface)
 
-    face(1:aint) = nptrs(localFaces(1:aint, iface))
+    face(1:aint) = obj%elementData(iel)%globalNodes(localFaces(1:aint, iface))
 
-    CALL FaceShapeMetaData_Quadrangle(face=face(1:aint),  &
-      & sorted_face=sorted_face(1:aint), faceOrient=faceOrient)
+    abool = IsQuadrangle(faceElemType(iface))
+    IF (abool) THEN
+      CALL FaceShapeMetaData_Quadrangle(face=face(1:aint),  &
+        & sorted_face=sorted_face(1:aint), faceOrient=faceOrient)
+    ELSE
+      CALL FaceShapeMetaData_Triangle(face=face(1:aint),  &
+        & sorted_face=sorted_face(1:aint), faceOrient=faceOrient)
+    END IF
 
     facePtr => FaceData_Pointer(sorted_face)
 
@@ -92,7 +102,7 @@ DO iel = 1, tElements
     CALL faceTree%Insert(facePtr)
     tsize2 = faceTree%SIZE()
 
-    obj%elementData(iel)%faceOrient(:, iface) = faceOrient
+    obj%elementData(iel)%faceOrient(:, iface) = INT(faceOrient, kind=INT8)
     obj%tFaces = tsize2
 
     IF (tsize1 .NE. tsize2) THEN
