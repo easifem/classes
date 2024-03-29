0eb7273bd43e0ebfb051d3171d68809da9a6161b
Vikas Sharma <vickysharma0812@gmail.com>
2024-03-29 09:59:29 +0900

    EASIFEM-134
    Adding import method in fedomain class

--- src/submodules/Domain/src/FEDomain_Class@IOMethods.F90
+++ src/submodules/Domain/src/FEDomain_Class@IOMethods.F90
@@ -167,4 +167,224 @@ CASE (0)
 END SELECT
 
 END PROCEDURE obj_DisplayDomainInfo
+
+!----------------------------------------------------------------------------
+!                                                                   Import
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_Import
+CHARACTER(*), PARAMETER :: myName = "FEDomain_Import()"
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & '[START] ')
+#endif
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & 'Calling FEDomainImportCheckErr()')
+#endif
+
+CALL FEDomainImportCheckErr(obj=obj, hdf5=hdf5, myName=myName)
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & 'Calling FEDomainImportMetaData')
+#endif
+
+CALL FEDomainImportMetaData(obj=obj, hdf5=hdf5, group=group, myName=myName)
+
+IF (obj%nsd .EQ. 3_I4B) THEN
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+    & 'Importing meshVolume')
+#endif
+
+  obj%meshVolume => FEMesh_Pointer()
+  CALL obj%meshVolume%Initiate(hdf5=hdf5, group=group, dim=3_I4B)
+  obj%tElements(3) = obj%meshVolume%GetTotalElements()
+END IF
+
+IF (obj%nsd .GT. 1_I4B) THEN
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+    & 'Importing meshSurface')
+#endif
+
+  obj%meshSurface => FEMesh_Pointer()
+  CALL obj%meshSurface%Initiate(hdf5=hdf5, group=group, dim=2_I4B)
+  obj%tElements(2) = obj%meshSurface%GetTotalElements()
+
+END IF
+
+IF (obj%nsd .GE. 1_I4B) THEN
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+    & 'Importing meshCurve')
+#endif
+
+  obj%meshCurve => FEMesh_Pointer()
+  CALL obj%meshCurve%Initiate(hdf5=hdf5, group=group, dim=1_I4B)
+  obj%tElements(1) = obj%meshCurve%GetTotalElements()
+
+END IF
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & 'Importing meshPoint')
+#endif
+
+obj%meshPoint => FEMesh_Pointer()
+CALL obj%meshPoint%Initiate(hdf5=hdf5, group=group, dim=0_I4B)
+obj%tElements(0) = obj%meshPoint%GetTotalElements()
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+  & '[END] ')
+#endif
+
+END PROCEDURE obj_Import
+
+!----------------------------------------------------------------------------
+!                                                     FEDomainImportCheckErr
+!----------------------------------------------------------------------------
+
+SUBROUTINE FEDomainImportCheckErr(obj, hdf5, myName)
+  CLASS(FEDomain_), INTENT(INOUT) :: obj
+  TYPE(HDF5File_), INTENT(INOUT) :: hdf5
+  CHARACTER(*), INTENT(IN) :: myName
+
+  ! internal variable
+  LOGICAL(LGT) :: problem
+
+  problem = obj%isInitiated
+
+  IF (problem) THEN
+    CALL e%RaiseError(modName//"::"//myName//" - "// &
+      & "[INTERNAL ERROR] :: FEDomain_Class::obj is already initiated.")
+    RETURN
+  END IF
+
+  problem = .NOT. hdf5%isOpen()
+  IF (problem) THEN
+    CALL e%RaiseError(modName//'::'//myName//" - "// &
+      & '[INTERNAL ERROR] :: HDF5 file is not opened')
+    RETURN
+  END IF
+
+  problem = .NOT. hdf5%isRead()
+  IF (problem) THEN
+    CALL e%RaiseError(modName//'::'//myName//" - "// &
+    & '[INTERNAL ERROR] :: HDF5 file does not have read permission')
+    RETURN
+  END IF
+END SUBROUTINE FEDomainImportCheckErr
+
+!----------------------------------------------------------------------------
+!                                                    FEDomainImportMetaData
+!----------------------------------------------------------------------------
+
+SUBROUTINE FEDomainImportMetaData(obj, hdf5, group, myName)
+  CLASS(FEDomain_), INTENT(INOUT) :: obj
+  TYPE(HDF5File_), INTENT(INOUT) :: hdf5
+  CHARACTER(*), INTENT(IN) :: group
+  CHARACTER(*), INTENT(IN) :: myName
+
+  obj%isInitiated = .TRUE.
+
+  ! read engine
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%engine, fieldname="engine", myName=myName, modName=modName)
+
+  ! read majorVersion
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%majorVersion, fieldname="majorVersion", myName=myName,  &
+    & modName=modName)
+
+  ! read minorVersion
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%minorVersion, fieldname="minorVersion", myName=myName,  &
+    & modName=modName)
+
+  ! read version
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%version, fieldname="version", myName=myName,  &
+    & modName=modName)
+
+  ! read NSD
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%NSD, fieldname="NSD", myName=myName,  &
+    & modName=modName)
+
+  ! maxNptrs
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%maxNptrs, fieldname="maxNptrs", myName=myName,  &
+    & modName=modName)
+
+  ! minNptrs
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%minNptrs, fieldname="minNptrs", myName=myName,  &
+    & modName=modName)
+
+  ! tNodes
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tNodes, fieldname="tNodes", myName=myName,  &
+    & modName=modName)
+
+  ! nodeCoord
+  CALL HDF5ReadMatrix(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%nodeCoord, fieldname="nodeCoord", myName=myName,  &
+    & modName=modName)
+
+  ! is node number sparse
+  IF ((obj%maxNptrs - obj%minNptrs) .EQ. (obj%tNodes - 1)) THEN
+    obj%isNodeNumberSparse = .FALSE.
+  ELSE
+    obj%isNodeNumberSparse = .TRUE.
+  END IF
+
+  ! maxElemNum
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%maxElemNum, fieldname="maxElemNum", myName=myName,  &
+    & modName=modName)
+
+  ! minElemNum
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%minElemNum, fieldname="minElemNum", myName=myName,  &
+    & modName=modName)
+
+  ! tEntitiesForNodes
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntitiesForNodes, fieldname="tEntitiesForNodes",  &
+    & myName=myName, modName=modName)
+
+  ! tEntitiesForElements
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntitiesForElements, fieldname="tEntitiesForElements",  &
+    & myName=myName, modName=modName)
+
+  ! numVolumeEntities
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntities(3), fieldname="numVolumeEntities",  &
+    & myName=myName, modName=modName)
+
+  ! numSurfaceEntities
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntities(2), fieldname="numSurfaceEntities",  &
+    & myName=myName, modName=modName)
+
+  ! numCurveEntities
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntities(1), fieldname="numCurveEntities",  &
+    & myName=myName, modName=modName)
+
+  ! numPointEntities
+  CALL HDF5ReadScalar(hdf5=hdf5, check=.TRUE., group=group,  &
+    & VALUE=obj%tEntities(0), fieldname="numPointEntities",  &
+    & myName=myName, modName=modName)
+
+END SUBROUTINE FEDomainImportMetaData
 END SUBMODULE IOMethods
