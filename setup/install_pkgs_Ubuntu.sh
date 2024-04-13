9de185136969c2173cfbf88c1c119aebb619d528
Vikas Sharma <vickysharma0812@gmail.com>
2024-04-13 23:38:29 +0900

    EAS-91 [wip] making things private in mesh

--- setup/install_pkgs_Ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This is a setup script for installing EASIFEM-base library.
-# (c) 2021, Dr Vikas Sharma, all rights reserved
-#
-#
-# Log (dd/mm/yyyy)
-# 15/02/2021 this document was created
-#
-#--------------------------------------------------------------
