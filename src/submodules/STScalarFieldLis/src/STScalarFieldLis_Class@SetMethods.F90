202fd969c556339567345260c0ce2e4acbf2e0e8
Vikas Sharma <vickysharma0812@gmail.com>
2024-06-08 12:20:48 +0900

    updates in stscalar field lis class set methods

--- src/submodules/STScalarFieldLis/src/STScalarFieldLis_Class@SetMethods.F90
+++ src/submodules/STScalarFieldLis/src/STScalarFieldLis_Class@SetMethods.F90
@@ -22,6 +22,10 @@ USE ScalarField_Class, ONLY: ScalarField_
 
 USE ScalarFieldLis_Class, ONLY: ScalarFieldLis_
 
+USE VectorField_Class, ONLY: VectorField_
+
+USE VectorFieldLis_Class, ONLY: VectorFieldLis_
+
 USE Display_Method, ONLY: ToString
 
 USE AbstractField_Class, ONLY: TypeField
@@ -58,81 +62,6 @@ CONTAINS
 !                                                                        Set
 !----------------------------------------------------------------------------
 
-! MODULE PROCEDURE obj_Set6
-! REAL(DFP), POINTER :: vecPointer(:)
-! CHARACTER(*), PARAMETER :: myName = "obj_Set6()"
-! INTEGER(I4B) :: ierr, tsize, s(3), code
-! REAL(DFP) :: areal
-! LOGICAL(LGT) :: abool
-!
-! #ifdef DEBUG_VER
-! INTEGER(I4B) :: tsize1
-!
-! #include "./lis_null_error.F90"
-!
-! CALL AssertError1(timeCompo .LE. obj%timeCompo, myName, &
-!                   "timeCompo is out of bound")
-!
-! CALL AssertError1(obj%fieldType .NE. TypeField%constant, myName, &
-!                   "Not callable for constant STScalar field")
-!
-! CALL AssertError2(obj%dof.tNodes.timeCompo, VALUE%dof.tNodes.1, myName, &
-!                   "a=obj%dof.tNodes.timeCompo, b=VALUE%dof.tNodes.1 ")
-! #endif
-!
-! SELECT TYPE (VALUE)
-!
-! TYPE IS (ScalarField_)
-!
-!   vecPointer => VALUE%GetPointer()
-!   CALL obj%Set(VALUE=vecPointer, timeCompo=timeCompo, &
-!                scale=scale, addContribution=addContribution)
-!   vecPointer => NULL()
-!
-! TYPE IS (ScalarFieldLis_)
-!
-! #ifdef DEBUG_VER
-!
-!   CALL lis_vector_is_null(VALUE%lis_ptr, ierr)
-!
-!   IF (ierr .EQ. LIS_TRUE) THEN
-!     CALL e%RaiseError(modName//'::'//myName//" - "// &
-!         "[INTERNAL ERROR] :: ScalarFieldLis_::value%lis_ptr is not available")
-!     RETURN
-!   END IF
-!
-! #endif
-!
-!   abool = Input(option=addContribution, default=.FALSE.)
-!   IF (abool) THEN; code = LIS_ADD_VALUE; ELSE; code = LIS_INS_VALUE; END IF
-!   areal = Input(option=scale, default=1.0_DFP)
-!   tsize = obj%dof.tNodes.timeCompo
-!   s = GetNodeLoc(obj=VALUE%dof, idof=1)
-!
-!   ! void lis_vector_set_values7_f(LIS_INT *flag, LIS_INT *start, LIS_INT *stride,
-!   ! LIS_INT *count, LIS_VECTOR_F *values,
-!   ! LIS_VECTOR_F *v, LIS_SCALAR *scale,
-!   ! LIS_INT *ierr)
-!
-!   CALL lis_vector_set_values7(code, s(1), s(3), tsize, VALUE%lis_ptr, &
-!                               obj%lis_ptr, areal, ierr)
-!
-! #ifdef DEBUG_VER
-!   CALL CHKERR(ierr)
-! #endif
-!
-! CLASS DEFAULT
-!   CALL e%RaiseError(modName//'::'//myName//' - '// &
-!                     '[INTERNAL ERROR] :: No case found for the type of Value')
-!   RETURN
-! END SELECT
-!
-! END PROCEDURE obj_Set6
-
-!----------------------------------------------------------------------------
-!                                                                        Set
-!----------------------------------------------------------------------------
-
 MODULE PROCEDURE obj_Set13
 CHARACTER(*), PARAMETER :: myName = "obj_Set13()"
 REAL(DFP), POINTER :: realvec(:)
@@ -146,25 +75,41 @@ IF (abool) THEN; code = LIS_ADD_VALUE; ELSE; code = LIS_INS_VALUE; END IF
 
 s = GetNodeLoc(obj=obj%dof, idof=GetIDOF(obj=obj%dof, ivar=ivar, idof=idof))
 
-! void lis_vector_set_values8_f(LIS_INT *flag, LIS_INT *start,
-! LIS_INT *stride,
-! LIS_INT *count, LIS_SCALAR *values,
-! LIS_VECTOR_F *v, LIS_SCALAR *scale,
-! LIS_INT *start_value, LIS_INT *stride_value,
-! LIS_INT *ierr)
-
 tsize = obj%dof.tNodes. [ivar, idof]
 
 SELECT TYPE (VALUE)
 
 TYPE IS (ScalarField_)
+  realvec => VALUE%GetPointer()
 
-  p = GetNodeLoc(obj=VALUE%dof, idof=1_I4B)
+  CALL obj%SetMultiple(VALUE=realvec, scale=scale, &
+         addContribution=addContribution, istart=s(1), iend=s(2), stride=s(3))
+
+  realvec => NULL()
+
+TYPE IS (STScalarField_)
+
+  p = GetNodeLoc(obj=VALUE%dof, idof=GetIDOF(obj=VALUE%dof, &
+                                            ivar=ivar_value, idof=idof_value))
 
   realvec => VALUE%GetPointer()
 
-  CALL lis_vector_set_values8(code, s(1), s(3), tsize, realvec, obj%lis_ptr, &
-                              areal, p(1), p(3), ierr)
+  CALL obj%SetMultiple(VALUE=realvec, scale=scale, &
+       addContribution=addContribution, istart=s(1), iend=s(2), stride=s(3), &
+                       istart_value=p(1), iend_value=p(2), stride_value=p(3))
+
+  realvec => NULL()
+
+TYPE IS (VectorField_)
+
+  p = GetNodeLoc(obj=VALUE%dof, idof=GetIDOF(obj=VALUE%dof, &
+                                            ivar=ivar_value, idof=idof_value))
+
+  realvec => VALUE%GetPointer()
+
+  CALL obj%SetMultiple(VALUE=realvec, scale=scale, &
+       addContribution=addContribution, istart=s(1), iend=s(2), stride=s(3), &
+                       istart_value=p(1), iend_value=p(2), stride_value=p(3))
 
   realvec => NULL()
 
@@ -174,15 +119,7 @@ TYPE IS (ScalarFieldLis_)
   CALL lis_vector_set_values9(code, s(1), s(3), tsize, VALUE%lis_ptr, &
                               obj%lis_ptr, areal, p(1), p(3), ierr)
 
-TYPE IS (STScalarField_)
-
-  p = GetNodeLoc(obj=VALUE%dof, idof=GetIDOF(obj=VALUE%dof, &
-                                            ivar=ivar_value, idof=idof_value))
-
-  realvec => VALUE%GetPointer()
-  CALL lis_vector_set_values8(code, s(1), s(3), tsize, realvec, obj%lis_ptr, &
-                              areal, p(1), p(3), ierr)
-  realvec => NULL()
+  CALL CHKERR(ierr)
 
 TYPE IS (STScalarFieldLis_)
 
@@ -191,6 +128,18 @@ TYPE IS (STScalarFieldLis_)
 
   CALL lis_vector_set_values9(code, s(1), s(3), tsize, VALUE%lis_ptr, &
                               obj%lis_ptr, areal, p(1), p(3), ierr)
+
+  CALL CHKERR(ierr)
+
+TYPE IS (VectorFieldLis_)
+
+  p = GetNodeLoc(obj=VALUE%dof, idof=GetIDOF(obj=VALUE%dof, &
+                                            ivar=ivar_value, idof=idof_value))
+
+  CALL lis_vector_set_values9(code, s(1), s(3), tsize, VALUE%lis_ptr, &
+                              obj%lis_ptr, areal, p(1), p(3), ierr)
+
+  CALL CHKERR(ierr)
 
 CLASS DEFAULT
   CALL e%RaiseError(modName//'::'//myName//' - '// &
@@ -209,13 +158,6 @@ CHARACTER(*), PARAMETER :: myName = "obj_Set14()"
 INTEGER(I4B) :: ierr
 REAL(DFP), POINTER :: realvec(:)
 
-#include "./lis_null_error.F90"
-
-#ifdef DEBUG_VER
-CALL AssertError1(obj%isInitiated, myName, &
-                  "STScalarFieldLis_::obj is not initiated")
-#endif
-
 SELECT TYPE (VALUE)
 
 TYPE IS (STScalarField_)
@@ -227,10 +169,7 @@ TYPE IS (STScalarField_)
 TYPE is (STScalarFieldLis_)
 
   CALL lis_vector_copy(VALUE%lis_ptr, obj%lis_ptr, ierr)
-
-#ifdef DEBUG_VER
   CALL CHKERR(ierr)
-#endif
 
 CLASS DEFAULT
 
