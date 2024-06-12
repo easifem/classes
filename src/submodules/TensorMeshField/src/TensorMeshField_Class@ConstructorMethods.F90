be76529bfb303a87a2bad96672b4022c9b5b9a5f
Vikas Sharma <vickysharma0812@gmail.com>
2024-06-12 09:18:49 +0900

    updates in tensor mesh field

--- src/submodules/TensorMeshField/src/TensorMeshField_Class@ConstructorMethods.F90
+++ src/submodules/TensorMeshField/src/TensorMeshField_Class@ConstructorMethods.F90
@@ -15,8 +15,16 @@
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 
 SUBMODULE(TensorMeshField_Class) ConstructorMethods
-USE BaseMethod
+USE GlobalData, ONLY: Constant, SpaceTime, Matrix, Nodal
+
+USE AbstractField_Class, ONLY: TypeField
+
+USE AbstractMeshField_Class, ONLY: SetAbstractMeshFieldParam
+
+USE Display_Method, ONLY: ToString
+
 IMPLICIT NONE
+
 CONTAINS
 
 !----------------------------------------------------------------------------
@@ -26,30 +34,23 @@ CONTAINS
 MODULE PROCEDURE SetTensorMeshFieldParam
 INTEGER(I4B) :: s(3), n
 CHARACTER(*), PARAMETER :: myName = "SetTensorMeshFieldParam()"
+LOGICAL(LGT) :: isok
 
-IF (varType .EQ. SpaceTime) THEN
-  CALL e%RaiseError(modName//'::'//myName//' - '// &
-    & '[CONFIG ERROR] :: For ScalarMeshField varType cannot be SpaceTime.'// &
-    & ' In this situation you should use STTensorMeshField_Class.')
-  RETURN
-END IF
+isok = varType .NE. SpaceTime
+CALL AssertError1(isok, myName, &
+                  'For ScalarMeshField varType cannot be SpaceTime.'// &
+                 ' In this situation you should use STTensorMeshField_Class.')
 
-IF (fieldType .EQ. FIELD_TYPE_CONSTANT) THEN
+IF (fieldType .EQ. TypeField%constant) THEN
   n = 2; s(1:n) = [dim1, dim2]
 ELSE
   n = 3; s(1:n) = [dim1, dim2, nns]
 END IF
 
-CALL SetAbstractMeshFieldParam( &
-  & param=param, &
-  & prefix=myPrefix, &
-  & name=name, &
-  & fieldType=fieldType, &
-  & varType=constant, &
-  & engine=engine, &
-  & defineOn=defineOn, &
-  & rank=Matrix, &
-  & s=s(1:n))
+CALL SetAbstractMeshFieldParam(param=param, prefix=myPrefix, &
+                           name=name, fieldType=fieldType, varType=constant, &
+                      engine=engine, defineOn=defineOn, rank=Matrix, s=s(1:n))
+
 END PROCEDURE SetTensorMeshFieldParam
 
 !----------------------------------------------------------------------------
@@ -59,34 +60,22 @@ END PROCEDURE SetTensorMeshFieldParam
 MODULE PROCEDURE obj_Initiate4
 CHARACTER(*), PARAMETER :: myName = "obj_Initiate4()"
 LOGICAL(LGT) :: isok
-INTEGER(I4B) :: returnType, argType, nns, varType, fieldType,  &
-  & numReturns, dims(2)
+INTEGER(I4B) :: returnType, argType, nns, varType, fieldType, &
+                numReturns, dims(2)
 TYPE(ParameterList_) :: param
-CLASS(ReferenceElement_), POINTER :: refelem
 
 #ifdef DEBUG_VER
 CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-  & '[START] ')
+                        '[START] ')
 #endif DEBUG_VER
 
-refelem => NULL()
-refelem => mesh%GetRefElemPointer()
-isok = ASSOCIATED(refelem)
-IF (.NOT. isok) THEN
-  CALL e%RaiseError(modName//'::'//myName//' - '// &
-    & '[INTERNAL ERROR] :: refelem pointer not found.')
-  RETURN
-END IF
-nns = (.NNE.refelem)
+nns = mesh%GetMaxNNE()
 
 returnType = func%GetReturnType()
 
 isok = returnType .EQ. Matrix
-IF (.NOT. isok) THEN
-  CALL e%RaiseError(modName//'::'//myName//' - '// &
-    & '[INTERNAL ERROR] :: returnType should be Matrix')
-  RETURN
-END IF
+CALL AssertError1(isok, myName, &
+                  'Return type should be Matrix.')
 
 argType = func%GetArgType()
 numReturns = func%GetNumReturns()
@@ -99,17 +88,18 @@ IF (argType .EQ. Constant) THEN
 END IF
 
 CALL param%Initiate()
-CALL SetTensorMeshFieldParam(param=param, name=name,  &
-   & fieldType=fieldType, varType=varType, engine=engine,  &
-   & defineOn=Nodal, dim1=dims(1), dim2=dims(2), nns=nns)
-CALL obj%Initiate(param=param, mesh=mesh)
-CALL param%DEALLOCATE()
 
-NULLIFY (refelem)
+CALL SetTensorMeshFieldParam(param=param, name=name, &
+                        fieldType=fieldType, varType=varType, engine=engine, &
+                          defineOn=Nodal, dim1=dims(1), dim2=dims(2), nns=nns)
+
+CALL obj%Initiate(param=param, mesh=mesh)
+
+CALL param%DEALLOCATE()
 
 #ifdef DEBUG_VER
 CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-  & '[END] ')
+                        '[END] ')
 #endif DEBUG_VER
 
 END PROCEDURE obj_Initiate4
