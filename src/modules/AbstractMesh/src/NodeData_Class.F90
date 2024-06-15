e4a73f64b7f04ddb0bd350a69fa29d8d64273ebc
Vikas Sharma <vickysharma0812@gmail.com>
2024-06-15 16:24:32 +0900

    updates in node data

--- src/modules/AbstractMesh/src/NodeData_Class.F90
+++ src/modules/AbstractMesh/src/NodeData_Class.F90
@@ -18,7 +18,10 @@
 MODULE NodeData_Class
 USE GlobalData, ONLY: I4B, DFP, LGT
 USE Display_Method, ONLY: Display
+USE AppendUtility, ONLY: Expand
+
 IMPLICIT NONE
+
 PRIVATE
 
 PUBLIC :: NodeData_
@@ -30,8 +33,33 @@ PUBLIC :: NodeData_lt
 PUBLIC :: NodeData_eq
 PUBLIC :: NodeData_SetID
 PUBLIC :: NodeData_Copy
+PUBLIC :: NodeData_GetGlobalNodeNum
+PUBLIC :: NodeData_GetLocalNodeNum
+PUBLIC :: NodeData_GetNodeType
+PUBLIC :: NodeData_GetTotalGlobalNodes
+PUBLIC :: NodeData_GetTotalExtraGlobalNodes
+PUBLIC :: NodeData_GetTotalGlobalElements
+PUBLIC :: NodeData_GetNodeCoord
+PUBLIC :: NodeData_GetGlobalNodes
+PUBLIC :: NodeData_GetExtraGlobalNodes
+PUBLIC :: NodeData_GetGlobalNodes2
+PUBLIC :: NodeData_GetGlobalElements
+PUBLIC :: NodeData_SetGlobalNodeNum
+PUBLIC :: NodeData_SetLocalNodeNum
+PUBLIC :: NodeData_SetNodeType
+PUBLIC :: NodeData_SetTotalGlobalNodes
+PUBLIC :: NodeData_SetTotalExtraGlobalNodes
+PUBLIC :: NodeData_SetTotalGlobalElements
+PUBLIC :: NodeData_SetNodeCoord
+PUBLIC :: NodeData_SetGlobalNodes
+PUBLIC :: NodeData_SetExtraGlobalNodes
+PUBLIC :: NodeData_SetGlobalElements
+PUBLIC :: NodeData_Set
+PUBLIC :: NodeData_ExpandGlobalElements
+PUBLIC :: NodeData_ExpandGlobalNodes
+PUBLIC :: NodeData_GetPointerToGlobalElements
+
 PUBLIC :: TypeNode
-PUBLIC :: NodeDataSet
 PUBLIC :: Display
 
 INTEGER(I4B), PARAMETER, PUBLIC :: INTERNAL_NODE = 1
@@ -52,6 +80,7 @@ END INTERFACE Display
 ! summary: Data type for storing node data
 
 TYPE :: NodeData_
+  PRIVATE
   INTEGER(I4B) :: globalNodeNum = 0
   !! Global node number
   INTEGER(I4B) :: localNodeNum = 0
@@ -63,10 +92,10 @@ TYPE :: NodeData_
   INTEGER(I4B), ALLOCATABLE :: globalNodes(:)
     !! It contains the global node number surrouding the node
     !! It does not contain self global node number
-  INTEGER(I4B), ALLOCATABLE :: globalElements(:)
-    !! It contains the global element number surrounding the node
   INTEGER(I4B), ALLOCATABLE :: extraGlobalNodes(:)
     !! These global nodes required in facet-element-data
+  INTEGER(I4B), ALLOCATABLE :: globalElements(:)
+    !! It contains the global element number surrounding the node
 END TYPE NodeData_
 
 !----------------------------------------------------------------------------
@@ -95,6 +124,175 @@ TYPE(NodeDataType_), PARAMETER :: TypeNode = NodeDataType_()
 !----------------------------------------------------------------------------
 
 CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalNodeNum
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetGlobalNodeNum(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = obj%globalNodeNum
+END FUNCTION NodeData_GetGlobalNodeNum
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalNodeNum
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetLocalNodeNum(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = obj%localNodeNum
+END FUNCTION NodeData_GetLocalNodeNum
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalNodeNum
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetNodeNum(obj, islocal) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  LOGICAL(LGT), INTENT(IN) :: islocal
+  INTEGER(I4B) :: ans
+
+  IF (islocal) THEN
+    ans = obj%localNodeNum
+  ELSE
+    ans = obj%globalNodeNum
+  END IF
+
+END FUNCTION NodeData_GetNodeNum
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalNodeNum
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetNodeType(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = obj%nodeType
+END FUNCTION NodeData_GetNodeType
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetTotalGlobalNodes(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = SIZE(obj%globalNodes)
+END FUNCTION NodeData_GetTotalGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetTotalExtraGlobalNodes(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = SIZE(obj%extraGlobalNodes)
+END FUNCTION NodeData_GetTotalExtraGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE FUNCTION NodeData_GetTotalGlobalElements(obj) RESULT(ans)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B) :: ans
+  ans = SIZE(obj%globalElements)
+END FUNCTION NodeData_GetTotalGlobalElements
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_GetNodeCoord(obj, ans, tsize)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  REAL(DFP), INTENT(INOUT) :: ans(:)
+  INTEGER(I4B), INTENT(OUT) :: tsize
+  tsize = 3
+  ans(1:tsize) = obj%nodeCoord(1:tsize)
+END SUBROUTINE NodeData_GetNodeCoord
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_GetGlobalNodes(obj, ans, tsize)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: ans(:)
+  INTEGER(I4B), INTENT(OUT) :: tsize
+
+  INTEGER(I4B) :: ii
+
+  tsize = SIZE(obj%globalNodes)
+  DO ii = 1, tsize
+    ans(ii) = obj%globalNodes(ii)
+  END DO
+END SUBROUTINE NodeData_GetGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                       GetExtraGlobalNodes
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_GetExtraGlobalNodes(obj, ans, tsize)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: ans(:)
+  INTEGER(I4B), INTENT(OUT) :: tsize
+
+  INTEGER(I4B) :: ii
+
+  tsize = SIZE(obj%extraGlobalNodes)
+  DO ii = 1, tsize
+    ans(ii) = obj%extraGlobalNodes(ii)
+  END DO
+END SUBROUTINE NodeData_GetExtraGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                       GetExtraGlobalNodes
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2024-06-15
+! summary: returns both the global nodes and extra global nodes
+
+PURE SUBROUTINE NodeData_GetGlobalNodes2(obj, ans, tsize)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: ans(:)
+  INTEGER(I4B), INTENT(OUT) :: tsize
+
+  INTEGER(I4B) :: ii, tsize0
+
+  tsize0 = SIZE(obj%globalNodes)
+
+  DO ii = 1, tsize0
+    ans(ii) = obj%globalNodes(ii)
+  END DO
+
+  tsize = SIZE(obj%extraGlobalNodes)
+  DO ii = 1, tsize
+    ans(ii + tsize0) = obj%extraGlobalNodes(ii)
+  END DO
+
+  tsize = tsize0 + tsize
+END SUBROUTINE NodeData_GetGlobalNodes2
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalElements
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_GetGlobalElements(obj, ans, tsize)
+  TYPE(NodeData_), INTENT(IN) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: ans(:)
+  INTEGER(I4B), INTENT(OUT) :: tsize
+
+  INTEGER(I4B) :: ii
+
+  tsize = SIZE(obj%globalElements)
+  DO ii = 1, tsize
+    ans(ii) = obj%globalElements(ii)
+  END DO
+END SUBROUTINE NodeData_GetGlobalElements
 
 !----------------------------------------------------------------------------
 !                                                                     Copy
@@ -221,20 +419,6 @@ FUNCTION NodeData_eq(obj, obj2) RESULT(ans)
 END FUNCTION NodeData_eq
 
 !----------------------------------------------------------------------------
-!                                                                  SetID
-!----------------------------------------------------------------------------
-
-!> author: Vikas Sharma, Ph. D.
-! date:  2024-01-29
-! summary:  Set the local node number
-
-SUBROUTINE NodeData_SetID(obj, id)
-  TYPE(NodeData_), INTENT(INOUT) :: obj
-  INTEGER(I4B), INTENT(IN) :: id
-  obj%localNodeNum = id
-END SUBROUTINE NodeData_SetID
-
-!----------------------------------------------------------------------------
 !                                                         NodeData_Pointer
 !----------------------------------------------------------------------------
 
@@ -255,7 +439,7 @@ END FUNCTION NodeData_Pointer
 ! date:  2024-01-29
 ! summary:  Set node data
 
-SUBROUTINE NodeDataSet(obj, globalNodeNum, localNodeNum, &
+SUBROUTINE NodeData_Set(obj, globalNodeNum, localNodeNum, &
            nodeType, globalNodes, globalElements, extraGlobalNodes, nodeCoord)
   TYPE(NodeData_), INTENT(INOUT) :: obj
   INTEGER(I4B), OPTIONAL, INTENT(IN) :: globalNodeNum
@@ -273,10 +457,204 @@ SUBROUTINE NodeDataSet(obj, globalNodeNum, localNodeNum, &
   IF (PRESENT(globalElements)) obj%globalElements = globalElements
   IF (PRESENT(extraGlobalNodes)) obj%extraGlobalNodes = extraGlobalNodes
   IF (PRESENT(nodeCoord)) obj%nodeCoord = nodeCoord
-END SUBROUTINE NodeDataSet
+END SUBROUTINE NodeData_Set
 
 !----------------------------------------------------------------------------
-!
+!                                                           SetGlobalNodeNum
 !----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2024-06-15
+! summary: Set the global node number
+
+PURE SUBROUTINE NodeData_SetGlobalNodeNum(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  obj%globalNodeNum = ans
+END SUBROUTINE NodeData_SetGlobalNodeNum
+
+!----------------------------------------------------------------------------
+!                                                           SetLocalNodeNum
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2024-06-15
+! summary: Set the lobal node number
+
+PURE SUBROUTINE NodeData_SetLocalNodeNum(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  obj%localNodeNum = ans
+END SUBROUTINE NodeData_SetLocalNodeNum
+
+!----------------------------------------------------------------------------
+!                                                           GetGlobalNodeNum
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetNodeType(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  obj%nodeType = ans
+END SUBROUTINE NodeData_SetNodeType
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetTotalGlobalNodes(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  ALLOCATE (obj%globalNodes(ans))
+END SUBROUTINE NodeData_SetTotalGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetTotalExtraGlobalNodes(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  ALLOCATE (obj%extraGlobalNodes(ans))
+END SUBROUTINE NodeData_SetTotalExtraGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                               GetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetTotalGlobalElements(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans
+  ALLOCATE (obj%globalElements(ans))
+END SUBROUTINE NodeData_SetTotalGlobalElements
+
+!----------------------------------------------------------------------------
+!                                                               SetNodeCoord
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetNodeCoord(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  REAL(DFP), INTENT(IN) :: ans(3)
+  obj%nodeCoord = ans
+END SUBROUTINE NodeData_SetNodeCoord
+
+!----------------------------------------------------------------------------
+!                                                             SetGlobalNodes
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetGlobalNodes(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans(:)
+
+  INTEGER(I4B) :: tsize, ii
+
+  tsize = SIZE(obj%globalNodes)
+  DO ii = 1, tsize
+    obj%globalNodes(ii) = ans(ii)
+  END DO
+END SUBROUTINE NodeData_SetGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                             SetGlobalNodes
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetExtraGlobalNodes(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans(:)
+
+  INTEGER(I4B) :: tsize, ii
+
+  tsize = SIZE(obj%extraGlobalNodes)
+  DO ii = 1, tsize
+    obj%extraGlobalNodes(ii) = ans(ii)
+  END DO
+END SUBROUTINE NodeData_SetExtraGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                             SetGlobalNodes
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_SetGlobalElements(obj, ans)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: ans(:)
+
+  INTEGER(I4B) :: tsize, ii
+
+  tsize = SIZE(obj%globalElements)
+  DO ii = 1, tsize
+    obj%globalElements(ii) = ans(ii)
+  END DO
+END SUBROUTINE NodeData_SetGlobalElements
+
+!----------------------------------------------------------------------------
+!                                                                  SetID
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date:  2024-01-29
+! summary:  Set the local node number
+
+SUBROUTINE NodeData_SetID(obj, id)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(IN) :: id
+  obj%localNodeNum = id
+END SUBROUTINE NodeData_SetID
+
+!----------------------------------------------------------------------------
+!                                                       ExpandGlobalElements
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_ExpandGlobalElements(obj, n, chunk_size, val, &
+                                              finished)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: n
+    !! counter for last element added to `vec`.
+    !! must be initialized to `size(vec)`
+    !! (or 0 if not allocated) before first call
+  INTEGER(I4B), INTENT(IN) :: chunk_size
+    !! allocate `vec` in blocks of this size (>0)
+  INTEGER(I4B), OPTIONAL, INTENT(IN) :: val
+    !! the value to add to `vec`
+  LOGICAL(LGT), OPTIONAL, INTENT(IN) :: finished
+    !! set to true to return `vec`
+    !! as its correct size (`n`)
+
+  CALL Expand(vec=obj%globalElements, n=n, chunk_size=chunk_size, &
+              val=val, finished=finished)
+
+END SUBROUTINE NodeData_ExpandGlobalElements
+
+!----------------------------------------------------------------------------
+!                                                       ExpandGlobalElements
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE NodeData_ExpandGlobalNodes(obj, n, chunk_size, val, &
+                                           finished)
+  TYPE(NodeData_), INTENT(INOUT) :: obj
+  INTEGER(I4B), INTENT(INOUT) :: n
+    !! counter for last element added to `vec`.
+    !! must be initialized to `size(vec)`
+    !! (or 0 if not allocated) before first call
+  INTEGER(I4B), INTENT(IN) :: chunk_size
+    !! allocate `vec` in blocks of this size (>0)
+  INTEGER(I4B), OPTIONAL, INTENT(IN) :: val
+    !! the value to add to `vec`
+  LOGICAL(LGT), OPTIONAL, INTENT(IN) :: finished
+    !! set to true to return `vec`
+    !! as its correct size (`n`)
+
+  CALL Expand(vec=obj%globalNodes, n=n, chunk_size=chunk_size, &
+              val=val, finished=finished)
+
+END SUBROUTINE NodeData_ExpandGlobalNodes
+
+!----------------------------------------------------------------------------
+!                                                GetPointerToGlobalElements
+!----------------------------------------------------------------------------
+
+FUNCTION NodeData_GetPointerToGlobalElements(obj) RESULT(ans)
+  TYPE(NodeData_), TARGET, INTENT(IN) :: obj
+  INTEGER(I4B), POINTER :: ans(:)
+  ans => obj%globalElements
+END FUNCTION NodeData_GetPointerToGlobalElements
 
 END MODULE NodeData_Class
