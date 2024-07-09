c13593534a948fd2b3d454b4a1630df0090c8017
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-10 08:58:33 +0900

    update in finite element class

--- src/submodules/FiniteElement/src/FiniteElement_Class@Methods.F90
+++ src/submodules/FiniteElement/src/FiniteElement_Class@Methods.F90
@@ -14,138 +14,138 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 
-SUBMODULE(FiniteElement_Class) ConstructorMethods
-USE ExceptionHandler_Class, ONLY: e
-USE ReallocateUtility, ONLY: Reallocate
-
-IMPLICIT NONE
-
-CONTAINS
-
-!----------------------------------------------------------------------------
-!                                                 FiniteElementInitiate
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE obj_Initiate1
-CHARACTER(*), PARAMETER :: myName = "obj_Initiate1()"
-TYPE(ParameterList_), POINTER :: sublist
-INTEGER(I4B) :: ierr, ii, tsize
-INTEGER(I4B), ALLOCATABLE :: elemType(:), order(:)
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START] ')
-#endif DEBUG_VER
-
-sublist => NULL()
-
-IF (.NOT. param%isSubList(key=myprefix)) RETURN
-
-ierr = param%GetSubList(key=myprefix, sublist=sublist)
-
-IF (ierr .NE. 0) THEN
-  CALL e%RaiseError(modName//'::'//myName//' - '// &
-                    '[INTERNAL ERROR] :: some error occured while getting'// &
-                    ' the sublist from param.')
-  RETURN
-END IF
-
-tsize = dom%GetTotalMesh(dim=dim)
-CALL Reallocate(elemType, tsize, order, tsize)
-elemType = dom%GetElemType(dim=dim)
-order = dom%GetOrder(dim=dim)
-tsize = SIZE(elemType)
-
-CALL FiniteElementDeallocate(obj)
-ALLOCATE (obj(tsize))
-
-DO ii = 1, tsize
-  ierr = sublist%Set(key=myprefix//"/elemType", VALUE=elemType(ii))
-  ierr = sublist%Set(key=myprefix//"/order", VALUE=order(ii))
-  ALLOCATE (FiniteElement_ :: obj(ii)%ptr)
-  CALL obj(ii)%ptr%Initiate(param=param)
-END DO
-
-sublist => NULL()
-IF (ALLOCATED(elemType)) DEALLOCATE (elemType)
-IF (ALLOCATED(order)) DEALLOCATE (order)
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[END] ')
-#endif DEBUG_VER
-
-END PROCEDURE obj_Initiate1
-
-!----------------------------------------------------------------------------
-!                                                                 Deallocate
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Deallocate_Vector
-#ifdef DEBUG_VER
-CHARACTER(*), PARAMETER :: myName = "Deallocate_Vector()"
-#endif
-
-INTEGER(I4B) :: ii
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START] ')
-#endif DEBUG_VER
-
-IF (ALLOCATED(obj)) THEN
-  DO ii = 1, SIZE(obj)
-    CALL obj(ii)%DEALLOCATE()
-  END DO
-  DEALLOCATE (obj)
-END IF
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[END] ')
-#endif DEBUG_VER
-
-END PROCEDURE Deallocate_Vector
-
-!----------------------------------------------------------------------------
-!                                                                 Deallocate
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Deallocate_Ptr_Vector
-#ifdef DEBUG_VER
-CHARACTER(*), PARAMETER :: myName = "Deallocate_Ptr_Vector()"
-#endif
-
-INTEGER(I4B) :: ii
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START] ')
-#endif DEBUG_VER
-
-IF (ALLOCATED(obj)) THEN
-  DO ii = 1, SIZE(obj)
-    IF (ASSOCIATED(obj(ii)%ptr)) THEN
-      CALL obj(ii)%ptr%DEALLOCATE()
-      obj(ii)%ptr => NULL()
-    END IF
-  END DO
-  DEALLOCATE (obj)
-END IF
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[END] ')
-#endif DEBUG_VER
-
-END PROCEDURE Deallocate_Ptr_Vector
-
-!----------------------------------------------------------------------------
+! SUBMODULE(FiniteElement_Class) ConstructorMethods
+! USE ExceptionHandler_Class, ONLY: e
+! USE ReallocateUtility, ONLY: Reallocate
 !
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE obj_GetPrefix
-ans = myprefix
-END PROCEDURE obj_GetPrefix
-
-END SUBMODULE ConstructorMethods
+! IMPLICIT NONE
+!
+! CONTAINS
+!
+! !----------------------------------------------------------------------------
+! !                                                 FiniteElementInitiate
+! !----------------------------------------------------------------------------
+!
+! MODULE PROCEDURE obj_Initiate1
+! CHARACTER(*), PARAMETER :: myName = "obj_Initiate1()"
+! TYPE(ParameterList_), POINTER :: sublist
+! INTEGER(I4B) :: ierr, ii, tsize
+! INTEGER(I4B), ALLOCATABLE :: elemType(:), order(:)
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[START] ')
+! #endif DEBUG_VER
+!
+! sublist => NULL()
+!
+! IF (.NOT. param%isSubList(key=myprefix)) RETURN
+!
+! ierr = param%GetSubList(key=myprefix, sublist=sublist)
+!
+! IF (ierr .NE. 0) THEN
+!   CALL e%RaiseError(modName//'::'//myName//' - '// &
+!                     '[INTERNAL ERROR] :: some error occured while getting'// &
+!                     ' the sublist from param.')
+!   RETURN
+! END IF
+!
+! tsize = dom%GetTotalMesh(dim=dim)
+! CALL Reallocate(elemType, tsize, order, tsize)
+! elemType = dom%GetElemType(dim=dim)
+! order = dom%GetOrder(dim=dim)
+! tsize = SIZE(elemType)
+!
+! CALL FiniteElementDeallocate(obj)
+! ALLOCATE (obj(tsize))
+!
+! DO ii = 1, tsize
+!   ierr = sublist%Set(key=myprefix//"/elemType", VALUE=elemType(ii))
+!   ierr = sublist%Set(key=myprefix//"/order", VALUE=order(ii))
+!   ALLOCATE (FiniteElement_ :: obj(ii)%ptr)
+!   CALL obj(ii)%ptr%Initiate(param=param)
+! END DO
+!
+! sublist => NULL()
+! IF (ALLOCATED(elemType)) DEALLOCATE (elemType)
+! IF (ALLOCATED(order)) DEALLOCATE (order)
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[END] ')
+! #endif DEBUG_VER
+!
+! END PROCEDURE obj_Initiate1
+!
+! !----------------------------------------------------------------------------
+! !                                                                 Deallocate
+! !----------------------------------------------------------------------------
+!
+! MODULE PROCEDURE Deallocate_Vector
+! #ifdef DEBUG_VER
+! CHARACTER(*), PARAMETER :: myName = "Deallocate_Vector()"
+! #endif
+!
+! INTEGER(I4B) :: ii
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[START] ')
+! #endif DEBUG_VER
+!
+! IF (ALLOCATED(obj)) THEN
+!   DO ii = 1, SIZE(obj)
+!     CALL obj(ii)%DEALLOCATE()
+!   END DO
+!   DEALLOCATE (obj)
+! END IF
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[END] ')
+! #endif DEBUG_VER
+!
+! END PROCEDURE Deallocate_Vector
+!
+! !----------------------------------------------------------------------------
+! !                                                                 Deallocate
+! !----------------------------------------------------------------------------
+!
+! MODULE PROCEDURE Deallocate_Ptr_Vector
+! #ifdef DEBUG_VER
+! CHARACTER(*), PARAMETER :: myName = "Deallocate_Ptr_Vector()"
+! #endif
+!
+! INTEGER(I4B) :: ii
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[START] ')
+! #endif DEBUG_VER
+!
+! IF (ALLOCATED(obj)) THEN
+!   DO ii = 1, SIZE(obj)
+!     IF (ASSOCIATED(obj(ii)%ptr)) THEN
+!       CALL obj(ii)%ptr%DEALLOCATE()
+!       obj(ii)%ptr => NULL()
+!     END IF
+!   END DO
+!   DEALLOCATE (obj)
+! END IF
+!
+! #ifdef DEBUG_VER
+! CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+!                         '[END] ')
+! #endif DEBUG_VER
+!
+! END PROCEDURE Deallocate_Ptr_Vector
+!
+! !----------------------------------------------------------------------------
+! !
+! !----------------------------------------------------------------------------
+!
+! MODULE PROCEDURE obj_GetPrefix
+! ans = myprefix
+! END PROCEDURE obj_GetPrefix
+!
+! END SUBMODULE ConstructorMethods
