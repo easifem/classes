758ca9b877b40c96d01d4b3f92827a2f2d5b4da2
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-11 12:18:44 +0900

    update in hierarchical fe class

---
+++ src/submodules/ScalarFE/src/HierarchicalFE_Class@Methods.F90
@@ -0,0 +1,141 @@
+! This program is a part of EASIFEM library
+! Expandable And Scalable Infrastructure for Finite Element Methods
+! htttps://www.easifem.com
+! Vikas Sharma, Ph.D., vickysharma0812@gmail.com
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(LagrangeFE_Class) Methods
+USE Display_Method, ONLY: ToString
+
+USE BaseType, ONLY: TypeElemNameOpt
+
+USE StringUtility, ONLY: UpperCase
+
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                         LagrangeFEPointer
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_LagrangeFEPointer
+ALLOCATE (ans)
+CALL ans%Initiate(elemType=elemType, nsd=nsd, baseContinuity=baseContinuity, &
+    baseInterpolation=baseInterpolation, ipType=ipType, basisType=basisType, &
+                  alpha=alpha, beta=beta, lambda=lambda, order=order, &
+                  anisoOrder=anisoOrder)
+END PROCEDURE obj_LagrangeFEPointer
+
+!----------------------------------------------------------------------------
+!                                                                GetPrefix
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_GetPrefix
+ans = myprefix
+END PROCEDURE obj_GetPrefix
+
+!----------------------------------------------------------------------------
+!                                                   FiniteElementDeallocate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE Deallocate_Vector
+INTEGER(I4B) :: ii
+
+IF (ALLOCATED(obj)) THEN
+  DO ii = 1, SIZE(obj)
+    CALL obj(ii)%DEALLOCATE()
+  END DO
+  DEALLOCATE (obj)
+END IF
+
+END PROCEDURE Deallocate_Vector
+
+!----------------------------------------------------------------------------
+!                                                                 Deallocate
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE Deallocate_Ptr_Vector
+INTEGER(I4B) :: ii
+
+IF (ALLOCATED(obj)) THEN
+  DO ii = 1, SIZE(obj)
+    IF (ASSOCIATED(obj(ii)%ptr)) THEN
+      CALL obj(ii)%ptr%DEALLOCATE()
+      obj(ii)%ptr => NULL()
+    END IF
+  END DO
+  DEALLOCATE (obj)
+END IF
+
+END PROCEDURE Deallocate_Ptr_Vector
+
+!----------------------------------------------------------------------------
+!                                                     GetLocalElemShapeData
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_GetLocalElemShapeData
+CHARACTER(*), PARAMETER :: myName = "obj_GetLocalElemShapeData()"
+! INTEGER(I4B) :: topoType
+
+CALL e%RaiseError(modName//'::'//myName//' - '// &
+                  '[WIP ERROR] :: This routine is under development')
+
+! topoType = obj%GetTopologyType()
+!
+! SELECT CASE (topoType)
+!
+! CASE (TypeElemNameOpt%line)
+! CASE (TypeElemNameOpt%triangle)
+! CASE (TypeElemNameOpt%quadrangle)
+! CASE (TypeElemNameOpt%tetrahedron)
+! CASE (TypeElemNameOpt%hexahedron)
+! CASE (TypeElemNameOpt%prism)
+! CASE (TypeElemNameOpt%pyramid)
+!
+! CASE DEFAULT
+!   CALL AssertError1(.FALSE., myname, &
+!                     'No case found for topoType = '//ToString(topoType))
+!   RETURN
+! END SELECT
+END PROCEDURE obj_GetLocalElemShapeData
+
+!----------------------------------------------------------------------------
+!                                                 GetLocalFacetElemShapeData
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_GetLocalFacetElemShapeData
+CHARACTER(*), PARAMETER :: myName = "obj_GetLocalFacetElemShapeData()"
+CALL e%RaiseError(modName//'::'//myName//' - '// &
+                  '[WIP ERROR] :: This routine is under development')
+END PROCEDURE obj_GetLocalFacetElemShapeData
+
+!----------------------------------------------------------------------------
+!                                                     GetGlobalElemShapeData
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_GetGlobalElemShapeData
+CHARACTER(*), PARAMETER :: myName = "obj_GetGlobalElemShapeData()"
+CALL e%RaiseError(modName//'::'//myName//' - '// &
+                  '[WIP ERROR] :: This routine is under development')
+END PROCEDURE obj_GetGlobalElemShapeData
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+#include "../../include/errors.F90"
+
+END SUBMODULE Methods
