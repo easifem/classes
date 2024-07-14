7a9dbc2e191d9cd5f55a1268300b30e8aa3caf76
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-14 22:52:43 +0900

    abstract fe class set methods

--- src/submodules/AbstractFE/src/AbstractFE_Class@SetMethods.F90
+++ src/submodules/AbstractFE/src/AbstractFE_Class@SetMethods.F90
@@ -15,7 +15,15 @@
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 
 SUBMODULE(AbstractFE_Class) SetMethods
+
+USE LagrangePolynomialUtility, ONLY: LagrangeDOF
+
+USE HierarchicalPolynomialUtility, ONLY: HierarchicalDOF
+
+USE ReallocateUtility, ONLY: Reallocate
+
 IMPLICIT NONE
+
 CONTAINS
 
 !----------------------------------------------------------------------------
@@ -69,6 +77,226 @@ CALL e%RaiseInformation(modName//'::'//myName//' - '// &
 #endif
 
 END PROCEDURE obj_SetParam
+
+!----------------------------------------------------------------------------
+!                                                                 SetOrder
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_SetOrder
+SELECT CASE (obj%baseInterpolation)
+CASE ("LAGR")
+  CALL obj%SetLagrangeOrder(order=order, anisoorder=anisoorder, &
+                            errCheck=errCheck)
+
+CASE ("HIER", "HEIR")
+  CALL obj%SetHierarchicalOrder(cellOrder=cellOrder, faceOrder=faceOrder, &
+                                edgeOrder=edgeOrder, cellOrient=cellOrient, &
+                               faceOrient=faceOrient, edgeOrient=edgeOrient, &
+                                errCheck=errCheck)
+
+END SELECT
+END PROCEDURE obj_SetOrder
+
+!----------------------------------------------------------------------------
+!                                                          SetLagrangeOrder
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_SetLagrangeOrder
+INTEGER(I4B), PARAMETER :: default_anisoOrder(3) = 0
+
+obj%isIsotropicOrder = PRESENT(order)
+
+IF (obj%isIsotropicOrder) THEN
+  obj%order = order
+  obj%tdof = LagrangeDOF(order=obj%order, elemType=obj%elemType)
+  CALL Reallocate(obj%coeff, obj%tdof, obj%tdof, isExpand=.TRUE., &
+                  expandFactor=2_I4B)
+  CALL Reallocate(obj%xij, 3, obj%tdof, isExpand=.TRUE., expandFactor=2_I4B)
+END IF
+
+obj%isAnisotropicOrder = PRESENT(anisoOrder)
+IF (obj%isAnisotropicOrder) THEN
+  CALL obj_SetIntegerType(a=obj%anisoOrder, default_a=default_anisoOrder, &
+                          b=anisoOrder, n=obj%xidim)
+
+  obj%tdof = LagrangeDOF(p=obj%anisoOrder(1), q=obj%anisoOrder(2), &
+                         r=obj%anisoOrder(3), elemType=obj%elemType)
+
+  CALL Reallocate(obj%coeff, obj%tdof, obj%tdof, isExpand=.TRUE., &
+                  expandFactor=2_I4B)
+  CALL Reallocate(obj%xij, 3, obj%tdof, isExpand=.TRUE., expandFactor=2_I4B)
+END IF
+END PROCEDURE obj_SetLagrangeOrder
+
+!----------------------------------------------------------------------------
+!                                                       SetHierarchicalOrder
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE obj_SetHierarchicalOrder
+#ifdef DEBUG_VER
+CHARACTER(*), PARAMETER :: myName = "obj_SetHierarchicalOrder()"
+LOGICAL(LGT) :: errCheck0
+#endif
+
+INTEGER(I4B) :: ii
+
+#ifdef DEBUG_VER
+errCheck0 = .FALSE.
+
+IF (errCheck) errCheck0 = errCheck
+
+IF (errCheck0) THEN
+  CALL checkerror
+END IF
+
+#endif
+
+obj%tdof = HierarchicalDOF(elemType=obj%elemType, cellOrder=cellOrder, &
+                           faceOrder=faceOrder, edgeOrder=edgeOrder)
+
+IF (PRESENT(cellOrder)) THEN
+
+  obj%isCellOrder = .TRUE.
+  obj%tCellOrder = SIZE(cellOrder)
+  DO ii = 1, obj%tCellOrder
+    obj%cellOrder(ii) = cellOrder(ii)
+    obj%cellOrient(ii) = cellOrient(ii)
+  END DO
+
+END IF
+
+IF (PRESENT(faceOrder)) THEN
+
+  IF (obj%xidim .GE. 2) THEN
+
+    obj%isFaceOrder = .TRUE.
+    obj%tFaceOrder = SIZE(faceOrder, 2)
+
+    DO ii = 1, obj%tFaceOrder
+      obj%faceOrder(1:3, ii) = faceOrder(1:3, ii)
+      obj%faceOrient(1:3, ii) = faceOrient(1:3, ii)
+    END DO
+
+  END IF
+
+END IF
+
+IF (PRESENT(edgeOrder)) THEN
+
+  IF (obj%xidim .GE. 3) THEN
+
+    obj%isEdgeOrder = .TRUE.
+    obj%tEdgeOrder = SIZE(edgeOrder)
+    DO ii = 1, obj%tEdgeOrder
+      obj%edgeOrder(ii) = edgeOrder(ii)
+      obj%edgeOrient(ii) = edgeOrient(ii)
+    END DO
+
+  END IF
+
+END IF
+
+#ifdef DEBUG_VER
+
+CONTAINS
+
+SUBROUTINE checkerror
+
+  LOGICAL(LGT) :: isok, abool
+
+  isok = PRESENT(cellOrder)
+  IF (.NOT. isok) THEN
+    CALL e%RaiseError(modName//'::'//myName//' - '// &
+                      '[INTERNAL ERROR] :: cellOrder is not present.')
+    RETURN
+  END IF
+
+  isok = PRESENT(cellOrient)
+  IF (.NOT. isok) THEN
+    CALL e%RaiseError(modName//'::'//myName//' - '// &
+                      '[INTERNAL ERROR] :: cellOrient is not present.')
+    RETURN
+  END IF
+
+  isok = SIZE(cellOrder) .EQ. SIZE(cellOrient)
+  IF (.NOT. isok) THEN
+    CALL e%RaiseError(modName//'::'//myName//' - '// &
+          '[INTERNAL ERROR] :: size of cellOrder and cellOrient is not same.')
+    RETURN
+  END IF
+
+  abool = obj%xidim .GE. 2
+  IF (abool) THEN
+    isok = PRESENT(faceOrder)
+
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                        '[INTERNAL ERROR] :: faceOrder is not present.')
+      RETURN
+    END IF
+
+    isok = SIZE(faceOrder, 1) .EQ. 3
+
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                      '[INTERNAL ERROR] :: rowsize in faceOrder should be 3.')
+      RETURN
+    END IF
+
+    isok = PRESENT(faceOrient)
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                        '[INTERNAL ERROR] :: faceOrient is not present.')
+      RETURN
+    END IF
+
+    isok = SIZE(faceOrient, 1) .EQ. 3
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                     '[INTERNAL ERROR] :: rowsize in faceOrient should be 3.')
+      RETURN
+    END IF
+
+    isok = SIZE(faceOrder, 2) .EQ. SIZE(faceOrient, 2)
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+          '[INTERNAL ERROR] :: colsize in faceOrder and faceOrient not same.')
+      RETURN
+    END IF
+
+  END IF
+
+  abool = obj%xidim .GE. 3
+  IF (abool) THEN
+    isok = PRESENT(edgeOrder)
+
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                        '[INTERNAL ERROR] :: edgeOrder is not present.')
+      RETURN
+    END IF
+
+    isok = PRESENT(edgeOrient)
+
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+                        '[INTERNAL ERROR] :: edgeOrient is not present.')
+      RETURN
+    END IF
+
+    isok = SIZE(edgeOrient) .EQ. SIZE(edgeOrder)
+    IF (.NOT. isok) THEN
+      CALL e%RaiseError(modName//'::'//myName//' - '// &
+          '[INTERNAL ERROR] :: size of edgeOrient and edgeOrder is not same.')
+      RETURN
+    END IF
+  END IF
+
+END SUBROUTINE checkerror
+
+#endif
+
+END PROCEDURE obj_SetHierarchicalOrder
 
 !----------------------------------------------------------------------------
 !
