a4cf533913d468b9dc383379e4cca3dc5316dfb0
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-27 21:24:47 +0900

    update in abstract bc

--- src/submodules/AbstractBC/src/AbstractBC_Class@GetMethods.F90
+++ src/submodules/AbstractBC/src/AbstractBC_Class@GetMethods.F90
@@ -23,6 +23,7 @@ USE GlobalData, ONLY: CHAR_LF
 USE AbstractMesh_Class, ONLY: AbstractMesh_
 
 IMPLICIT NONE
+
 CONTAINS
 
 !----------------------------------------------------------------------------
@@ -54,86 +55,31 @@ END PROCEDURE obj_GetDOFNo
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE obj_Get1
-#ifdef DEBUG_VER
 CHARACTER(*), PARAMETER :: myName = "obj_Get1()"
-#endif
-
-INTEGER(I4B) :: ii, jj
-LOGICAL(LGT) :: isok
+CHARACTER(6) :: casename
 
 #ifdef DEBUG_VER
 CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START]')
+                        '[START] ')
 #endif
 
-#ifdef DEBUG_VER
-CALL AssertError1(obj%isInitiated, myName, &
-                  'AbstractBC_ object is not initiated, initiate it first.')
+casename = fedof%GetCaseName()
 
-isok = ASSOCIATED(obj%dom)
-CALL AssertError1(isok, myName, &
-                  'AbstractBC_::obj%dom is not associated!')
-#endif
+SELECT CASE (casename)
 
-CALL obj%boundary%GetNodeNum(dom=obj%dom, ans=nodenum, tsize=nrow)
+CASE ("H1LAGR")
 
-IF (obj%isUserFunction) THEN
-  CALL GetFromUserFunction(obj=obj, nodeNum=nodeNum, nodalValue=nodalValue, &
-                           times=times, nrow=nrow, ncol=ncol)
+  CALL obj%GetH1Lagrange(fedof=fedof, nodenum=nodenum, &
+                     nodalValue=nodalValue, nrow=nrow, ncol=ncol, times=times)
+
+CASE ("H1HIER", "H1HEIR")
+
+  CALL obj%GetH1Hierarchical(fedof=fedof, nodenum=nodenum, &
+                     nodalValue=nodalValue, nrow=nrow, ncol=ncol, times=times)
+
+CASE DEFAULT
+  CALL AssertError1(.FALSE., myname, "No case found for fedof casename")
   RETURN
-END IF
-
-ncol = 1
-IF (PRESENT(times)) ncol = SIZE(times)
-
-#ifdef DEBUG_VER
-isok = ALLOCATED(obj%nodalValue)
-CALL AssertError1(isok, myname, &
-                  'AbstractBC_::obj%nodalValue is not allocated!')
-#endif
-
-! get nodal values
-SELECT CASE (obj%nodalValueType)
-
-CASE (TypeFEVariableOpt%constant)
-
-  nodalValue(1:nrow, 1:ncol) = obj%nodalValue(1, 1)
-
-CASE (TypeFEVariableOpt%space)
-
-#ifdef DEBUG_VER
-  isok = obj%nrow .GE. nrow
-  CALL AssertError1(isok, myname, &
-                    'SIZE( obj%nodalValue, 1 ) .NE. SIZE( nodeNum )')
-#endif
-
-  ncol = 1
-  DO CONCURRENT(ii=1:nrow)
-    nodalValue(ii, 1) = obj%nodalValue(ii, 1)
-  END DO
-
-! Time
-CASE (TypeFEVariableOpt%time)
-  ncol = obj%nrow
-
-  DO CONCURRENT(ii=1:nrow, jj=1:ncol)
-    nodalValue(ii, jj) = obj%nodalValue(jj, 1)
-  END DO
-
-! SpaceTime
-CASE (TypeFEVariableOpt%spacetime)
-
-#ifdef DEBUG_VER
-  isok = obj%nrow .GE. nrow
-  CALL AssertError1(isok, myname, &
-                    'SIZE(obj%nodalValue, 1) .NE. SIZE(nodeNum)')
-#endif
-
-  ncol = obj%ncol
-
-  DO CONCURRENT(ii=1:nrow, jj=1:ncol)
-    nodalValue(ii, jj) = obj%nodalValue(ii, jj)
-  END DO
 
 END SELECT
 
@@ -148,26 +94,31 @@ END PROCEDURE obj_Get1
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE obj_Get2
-#ifdef DEBUG_VER
-CHARACTER(*), PARAMETER :: myName = "obj_Get2()"
-LOGICAL(LGT) :: isok
-#endif
+CHARACTER(*), PARAMETER :: myName = "obj_Get1()"
+CHARACTER(6) :: casename
 
 #ifdef DEBUG_VER
 CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START]')
+                        '[START] ')
 #endif
 
-#ifdef DEBUG_VER
-CALL AssertError1(obj%isInitiated, myName, &
-                  'AbstractBC_ object is not initiated, initiate it first.')
+casename = fedof%GetCaseName()
 
-isok = ASSOCIATED(obj%dom)
-CALL AssertError1(isok, myName, &
-                  'AbstractBC_::obj%dom is not associated!')
-#endif
+SELECT CASE (casename)
 
-CALL obj%boundary%GetNodeNum(dom=obj%dom, ans=nodenum, tsize=tsize)
+CASE ("H1LAGR")
+
+  CALL obj%GetH1Lagrange(fedof=fedof, nodenum=nodenum, tsize=tsize)
+
+CASE ("H1HIER", "H1HEIR")
+
+  CALL obj%GetH1Hierarchical(fedof=fedof, nodenum=nodenum, tsize=tsize)
+
+CASE DEFAULT
+  CALL AssertError1(.FALSE., myname, "No case found for fedof casename")
+  RETURN
+
+END SELECT
 
 #ifdef DEBUG_VER
 CALL e%RaiseInformation(modName//'::'//myName//' - '// &
@@ -176,29 +127,40 @@ CALL e%RaiseInformation(modName//'::'//myName//' - '// &
 END PROCEDURE obj_Get2
 
 !----------------------------------------------------------------------------
-!                                                           obj_GetFEVar
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE obj_Get3
-CHARACTER(*), PARAMETER :: myName = "obj_Get3()"
-CALL e%RaiseError(modName//'::'//myName//' - '// &
-                  '[WIP ERROR] :: This routine is under development.')
-
-! If useFunction is true  then
-!    if constant
-!    if time
-!    if space
-!    if space-time
-
-! If useFunction is not true then
-END PROCEDURE obj_Get3
-
-!----------------------------------------------------------------------------
 !                                                           GetTotalNodeNum
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE obj_GetTotalNodenum
-ans = obj%boundary%GetTotalNodeNum(obj%dom)
+CHARACTER(*), PARAMETER :: myName = "obj_GetTotalNodenum()"
+CHARACTER(6) :: casename
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                        '[START] ')
+#endif
+
+casename = fedof%GetCaseName()
+
+SELECT CASE (casename)
+
+CASE ("H1LAGR")
+
+  ans = obj%GetTotalNodeNumH1Lagrange(fedof=fedof)
+
+CASE ("H1HIER", "H1HEIR")
+
+  ans = obj%GetTotalNodeNumH1Hierarchical(fedof=fedof)
+
+CASE DEFAULT
+  CALL AssertError1(.FALSE., myname, "No case found for fedof casename")
+  RETURN
+
+END SELECT
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                        '[END]')
+#endif
 END PROCEDURE obj_GetTotalNodenum
 
 !----------------------------------------------------------------------------
@@ -238,199 +200,6 @@ CHARACTER(*), PARAMETER :: myName = "obj_GetPrefix()"
 CALL e%RaiseError(modName//'::'//myName//' - '// &
           '[WIP ERROR] :: This routine should be implemented by child class.')
 END PROCEDURE obj_GetPrefix
-
-!----------------------------------------------------------------------------
-!                                                            GetFromFunction
-!----------------------------------------------------------------------------
-
-!> author: Vikas Sharma, Ph. D.
-! date:
-! summary:  This function returns the nodalValue from userfunction
-!
-!# Introduction
-!
-! the shape and value of nodal value depends upon the obj%nodalValuetype
-!
-! constant: shape of nodalvalue is tnodes, 1
-! in this case we get the value from function and set
-! all values of nodalvalue(:, 1)
-!
-! space:
-! ncol = 1
-!
-! time
-! ncol = ttimes
-!
-! spacetime
-! ncol = ttimes
-
-SUBROUTINE GetFromUserFunction(obj, nodeNum, nodalValue, &
-                               nrow, ncol, times)
-  CLASS(AbstractBC_), INTENT(INOUT) :: obj
-  INTEGER(I4B), INTENT(IN) :: nodeNum(:)
-    !! node number should be allocated
-    !! size of nodenum is nrow
-  REAL(DFP), INTENT(INOUT) :: nodalValue(:, :)
-    !! nodal values
-  REAL(DFP), OPTIONAL, INTENT(IN) :: times(:)
-    !! time value
-    !! time values are needed when userfunction is time or space-time
-  INTEGER(I4B) :: nrow, ncol
-    !! nrow is size of nodenum and size of number of rows in nodalvalue
-    !! ncol is colsize of nodalvalue
-
-    !! Internal variables
-  CHARACTER(*), PARAMETER :: myName = "GetFromUserFunction()"
-  INTEGER(I4B) :: ii, kk, retType, tnodes, nsd, tTimes, argType, &
-                  tsize
-  REAL(DFP) :: xij(4, 1), ans
-  LOGICAL(LGT) :: problem
-  CLASS(AbstractMesh_), POINTER :: meshptr
-
-! get pointer to nodecoord
-
-#ifdef DEBUG_VER
-  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                          '[START] ')
-#endif
-
-#ifdef DEBUG_VER
-  problem = .NOT. ASSOCIATED(obj%func)
-  IF (problem) THEN
-    CALL e%RaiseError(modName//'::'//myName//" - "// &
-                      "[INTERNAL ERROR] :: When nodalValueType is "// &
-                      CHAR_LF//"Space and useFunction is specified, "// &
-                      CHAR_LF//"then SpaceFunction is needed, "// &
-                      CHAR_LF//"but it is not associated")
-    RETURN
-  END IF
-#endif
-
-  retType = obj%func%GetReturnType()
-
-#ifdef DEBUG_VER
-  problem = retType .NE. TypeFEVariableOpt%scalar
-  IF (problem) THEN
-    CALL e%RaiseError(modName//'::'//myName//' - '// &
-             '[INTERNAL ERROR] :: Return type of user function should be '// &
-                      'scalar.')
-    RETURN
-  END IF
-
-  problem = (obj%nodalValueType .EQ. TypeFEVariableOpt%time) .AND. &
-            (.NOT. PRESENT(times))
-  IF (problem) THEN
-    CALL e%RaiseError(modName//'::'//myName//" - "// &
-                      "[INTERNAL ERROR] :: When `nodalValueType` is Time "// &
-                      " and `IsUserFunction` is TRUE, "// &
-                      " then `times` is needed in the passing argument,"// &
-                      " but it is not present")
-    RETURN
-  END IF
-
-  problem = (obj%nodalValueType .EQ. TypeFEVariableOpt%spacetime) .AND. &
-            (.NOT. PRESENT(times))
-  IF (problem) THEN
-    CALL e%RaiseError(modName//'::'//myName//" - "// &
-                 "[INTERNAL ERROR] :: When `nodalValueType` is SpaceTime "// &
-                      " and `IsUserFunction` is TRUE, "// &
-                      " then `times` is needed in the passing argument,"// &
-                      " but it is not present")
-    RETURN
-  END IF
-#endif
-
-  argType = obj%func%GetArgType()
-
-#ifdef DEBUG_VER
-  problem = argType .NE. obj%nodalValueType
-  IF (problem) THEN
-    CALL e%RaiseError(modName//'::'//myName//' - '// &
-                      '[INTERNAL ERROR] :: argType='//tostring(argType)// &
-                      ' in user function is not same '// &
-                      'as nodalValueType '//tostring(obj%nodalValueType)// &
-                      ' in AbstractBC_')
-    RETURN
-  END IF
-#endif
-
-  tnodes = SIZE(nodeNum)
-  nrow = tnodes
-  ncol = 0
-  nsd = obj%dom%GetNSD()
-
-  SELECT CASE (obj%nodalValueType)
-
-! Constant
-  CASE (TypeFEVariableOpt%constant)
-    ! CALL Reallocate(nodalValue, tnodes, 1)
-    ncol = 1
-    CALL obj%func%Get(val=ans)
-    nodalValue(1:nrow, 1) = ans
-
-! Space
-  CASE (TypeFEVariableOpt%space)
-    ! CALL Reallocate(nodalValue, tnodes, 1)
-    ncol = 1
-    meshptr => obj%dom%GetMeshPointer(dim=nsd)
-
-    DO ii = 1, tnodes
-      CALL meshptr%GetNodeCoord(nodeCoord=xij(:, 1), tsize=tsize, &
-                                globalNode=nodeNum(ii), islocal=.FALSE.)
-
-      CALL obj%func%Get(val=ans, args=xij(1:3, 1))
-
-      nodalValue(ii, 1) = ans
-    END DO
-
-    meshptr => NULL()
-
-! Time
-  CASE (TypeFEVariableOpt%time)
-
-    tTimes = SIZE(times)
-
-    ! CALL Reallocate(nodalValue, tnodes, tTimes)
-    ncol = tTimes
-
-    DO ii = 1, tTimes
-      CALL obj%func%Get(val=ans, args=times(ii:ii))
-
-      nodalValue(1:nrow, ii) = ans
-    END DO
-
-! SpaceTime
-  CASE (TypeFEVariableOpt%spacetime)
-    tTimes = SIZE(times)
-
-    ! CALL Reallocate(nodalValue, tnodes, tTimes)
-    ncol = tTimes
-
-    meshptr => obj%dom%GetMeshPointer(dim=nsd)
-
-    DO kk = 1, tTimes
-      xij(nsd + 1, 1) = times(kk)
-
-      DO ii = 1, tnodes
-        CALL meshptr%GetNodeCoord(nodeCoord=xij(:, 1), tsize=tsize, &
-                                  globalNode=nodeNum(ii), islocal=.FALSE.)
-
-        CALL obj%func%Get(val=ans, args=xij(1:4, 1))
-
-        nodalValue(ii, kk) = ans
-      END DO
-    END DO
-
-    meshptr => NULL()
-
-  END SELECT
-
-#ifdef DEBUG_VER
-  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                          '[END] ')
-#endif
-
-END SUBROUTINE GetFromUserFunction
 
 !----------------------------------------------------------------------------
 !
