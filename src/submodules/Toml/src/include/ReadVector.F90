bafcc7e1aa2841eaf6795b17708b51040ca2ae7b
easifem <vickysharma0812@gmail.com>
2025-05-15 12:04:06 +0900

    Removing 1D kernels from classes
    Removing 1D elastodynamics kernel from the classes.
    Improving docs of TomlUtility

--- src/submodules/Toml/src/include/ReadVector.F90
+++ src/submodules/Toml/src/include/ReadVector.F90
@@ -14,77 +14,92 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 
-      CHARACTER(*), PARAMETER :: myName = "toml_get"
-      TYPE(toml_array), POINTER :: array
-      INTEGER(I4B) :: tsize, stat0, iostat, ii
-      TYPE(String) :: filename
-      TYPE(TxtFile_) :: atxtfile
-      CHARACTER(512) :: iomsg
-      LOGICAL(LGT) :: isFound0, bool1, isok
+CHARACTER(*), PARAMETER :: myName = "toml_get"
+TYPE(toml_array), POINTER :: array
+INTEGER(I4B) :: tsize, stat0, iostat, ii
+TYPE(String) :: filename
+TYPE(TxtFile_) :: atxtfile
+CHARACTER(512) :: iomsg
+LOGICAL(LGT) :: isFound0, bool1, isok
 
-      isFound0 = .FALSE.
+isFound0 = .FALSE.
 
-      ! try to read from the array
-      array => NULL()
-      CALL toml_get(table, key, array, origin=origin, stat=stat0, &
-                    requested=.FALSE.)
+!----------------------------------------------------------------------------
+! READ from TOML array
+! try to read from the toml array
+! the data is given in toml file itself as toml array
+!----------------------------------------------------------------------------
+array => NULL()
+CALL toml_get(table, key, array, origin=origin, stat=stat0, &
+              requested=.FALSE.)
 
-      isok = ASSOCIATED(array)
+isok = ASSOCIATED(array)
 
-      IF (isok) THEN
-        tsize = toml_len(array)
-        CALL Reallocate(VALUE, tsize)
-        isFound0 = .TRUE.
-        DO ii = 1, tsize
-          CALL toml_get(array, ii, VALUE(ii))
-        END DO
+IF (isok) THEN
+  tsize = toml_len(array)
+  CALL Reallocate(VALUE, tsize)
+  isFound0 = .TRUE.
+  DO ii = 1, tsize
+    CALL toml_get(array, ii, VALUE(ii))
+  END DO
 
-        IF (PRESENT(stat)) stat = stat0
-        IF (PRESENT(isFound)) isFound = isFound0
-        NULLIFY (array)
-        RETURN
-      END IF
+  IF (PRESENT(stat)) stat = stat0
+  IF (PRESENT(isFound)) isFound = isFound0
+  NULLIFY (array)
+  RETURN
+END IF
 
-      ! try to read from the file
-      CALL toml_get(table, key, filename%raw, origin=origin, stat=stat0)
+!----------------------------------------------------------------------------
+! READ from a txt file
+! try to read from the file
+! the data is given in a txt file 
+! the filename is given in the toml file
+!----------------------------------------------------------------------------
 
-      IF (stat0 .EQ. toml_stat%success) THEN
-        CALL atxtfile%Initiate(filename=filename%Chars(), &
-                               action="READ", status="OLD")
-        CALL atxtfile%OPEN()
-        CALL atxtfile%READ(val=VALUE, iostat=iostat, iomsg=iomsg)
+CALL toml_get(table, key, filename%raw, origin=origin, stat=stat0)
 
-        bool1 = iostat .NE. 0 .AND. (.NOT. atxtfile%isEOF())
-        IF (bool1) THEN
-          CALL e%RaiseError(modName//'::'//myName//' - '// &
-               '[INTERNAL ERROR] :: Error while reading txtfile, errmsg= '// &
-                            CHAR_LF//TRIM(iomsg))
-          IF (PRESENT(isFound)) isFound = isFound0
-          IF (PRESENT(stat)) stat = stat0
-          filename = ""
-          RETURN
-        END IF
+IF (stat0 .EQ. toml_stat%success) THEN
+  CALL atxtfile%Initiate(filename=filename%Chars(), &
+                          action="READ", status="OLD")
+  CALL atxtfile%OPEN()
+  CALL atxtfile%READ(val=VALUE, iostat=iostat, iomsg=iomsg)
 
-        isFound0 = .TRUE.
-        CALL atxtfile%DEALLOCATE()
+  bool1 = iostat .NE. 0 .AND. (.NOT. atxtfile%isEOF())
+  IF (bool1) THEN
+    CALL e%RaiseError(modName//'::'//myName//' - '// &
+          '[INTERNAL ERROR] :: Error while reading txtfile, errmsg= '// &
+                      CHAR_LF//TRIM(iomsg))
+    IF (PRESENT(isFound)) isFound = isFound0
+    IF (PRESENT(stat)) stat = stat0
+    filename = ""
+    RETURN
+  END IF
 
-        IF (PRESENT(isFound)) isFound = isFound0
-        IF (PRESENT(stat)) stat = stat0
-        filename = ""
-        RETURN
-      END IF
+  isFound0 = .TRUE.
+  CALL atxtfile%DEALLOCATE()
 
-      CALL toml_get(table, key, temp, origin=origin, stat=stat0)
+  IF (PRESENT(isFound)) isFound = isFound0
+  IF (PRESENT(stat)) stat = stat0
+  filename = ""
+  RETURN
+END IF
 
-      IF (stat0 .EQ. toml_stat%success) THEN
-        CALL Reallocate(VALUE, 1)
-        VALUE(1) = temp
-        isFound0 = .TRUE.
-        IF (PRESENT(isFound)) isFound = isFound0
-        IF (PRESENT(stat)) stat = stat0
-        RETURN
-      END IF
+!----------------------------------------------------------------------------
+! READ a scalar value from toml
+! In this case length of the vector is 1, this value is given in toml file
+!----------------------------------------------------------------------------
 
-      isFound0 = .FALSE.
-      IF (PRESENT(isFound)) isFound = isFound0
-      IF (PRESENT(stat)) stat = stat0
+CALL toml_get(table, key, temp, origin=origin, stat=stat0)
+
+IF (stat0 .EQ. toml_stat%success) THEN
+  CALL Reallocate(VALUE, 1)
+  VALUE(1) = temp
+  isFound0 = .TRUE.
+  IF (PRESENT(isFound)) isFound = isFound0
+  IF (PRESENT(stat)) stat = stat0
+  RETURN
+END IF
+
+isFound0 = .FALSE.
+IF (PRESENT(isFound)) isFound = isFound0
+IF (PRESENT(stat)) stat = stat0
