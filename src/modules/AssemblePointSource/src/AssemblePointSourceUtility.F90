5d125acc8a6df76f99eb47993122fe9439bc2886
Vikas Sharma <vickysharma0812@gmail.com>
2025-09-06 12:36:33 +0900

    adding AssemblePointSourceUtility
    adding AssemblePointSource

---
+++ src/modules/AssemblePointSource/src/AssemblePointSourceUtility.F90
@@ -0,0 +1,65 @@
+! This program is a part of EASIFEM library
+! Expandable And Scalable Infrastructure for Finite Element Methods
+! htttps://www.easifem.com
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+MODULE AssemblePointSourceUtility
+USE GlobalData, ONLY: DFP, LGT, I4B
+USE ExceptionHandler_Class, ONLY: e
+USE NeumannBC_Class, ONLY: NeumannBC_, NeumannBCPointer_
+USE ScalarField_Class, ONLY: ScalarField_
+USE FEDOF_Class, ONLY: FEDOF_
+
+IMPLICIT NONE
+PRIVATE
+
+PUBLIC :: ScalarFieldAssemblePointSource
+
+CHARACTER(*), PARAMETER :: modName = "AssemblePointSourceUtility"
+
+!----------------------------------------------------------------------------
+!                                                                 DefaultOpt_
+!----------------------------------------------------------------------------
+
+TYPE :: DefaultOpt_
+  REAL(DFP) :: one = 1.0_DFP, zero = 0.0_DFP, minus_one = -1.0_DFP, &
+               half = 0.5_DFP
+  LOGICAL(LGT) :: yes = .TRUE.
+  LOGICAL(LGT) :: no = .FALSE.
+END TYPE DefaultOpt_
+
+TYPE(DefaultOpt_), PARAMETER :: defaultOpt = DefaultOpt_()
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2025-09-05
+! summary: Assemble RHS
+
+INTERFACE ScalarFieldAssemblePointSource
+  MODULE SUBROUTINE ScalarFieldAssemblePointSource1(rhs, nbc, fedof, reset, &
+                                                    scale)
+    CLASS(ScalarField_), INTENT(INOUT) :: rhs
+    CLASS(NeumannBCPointer_), INTENT(INOUT) :: nbc(:)
+    CLASS(FEDOF_), INTENT(INOUT) :: fedof
+    LOGICAL(LGT), INTENT(IN) :: reset
+    REAL(DFP), INTENT(IN) :: scale
+  END SUBROUTINE ScalarFieldAssemblePointSource1
+END INTERFACE ScalarFieldAssemblePointSource
+
+END MODULE AssemblePointSourceUtility
---
+++ src/submodules/AssemblePointSource/src/AssemblePointSourceUtility@Methods.F90
@@ -0,0 +1,93 @@
+! This program is a part of EASIFEM library
+! Expandable And Scalable Infrastructure for Finite Element Methods
+! htttps://www.easifem.com
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(AssemblePointSourceUtility) Methods
+USE Display_Method, ONLY: ToString
+USE ReallocateUtility, ONLY: Reallocate
+
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                              ScalarFieldAssemblePointSource
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE ScalarFieldAssemblePointSource1
+#ifdef DEBUG_VER
+CHARACTER(*), PARAMETER :: myName = "ScalarFieldAssemblePointSource1()"
+#endif
+
+CLASS(NeumannBC_), POINTER :: nbcptr
+LOGICAL(LGT) :: isok
+INTEGER(I4B) :: tnbc, inbc, ii, tsize, jj, totalNodes
+REAL(DFP), ALLOCATABLE :: nodalValue(:, :)
+INTEGER(I4B), ALLOCATABLE :: nodeNum(:)
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                        '[START] ')
+#endif
+
+tnbc = SIZE(nbc)
+
+totalNodes = 0
+DO inbc = 1, tnbc
+  nbcptr => nbc(inbc)%ptr
+  isok = ASSOCIATED(nbcptr)
+  IF (.NOT. isok) CYCLE
+  ii = nbcptr%GetTotalNodeNum(fedof=fedof)
+  totalNodes = MAX(totalNodes, ii)
+END DO
+
+CALL Reallocate(nodeNum, totalNodes)
+CALL Reallocate(nodalValue, totalNodes, 1)
+
+DO inbc = 1, tnbc
+  nbcptr => nbc(inbc)%ptr
+  isok = ASSOCIATED(nbcptr)
+  IF (.NOT. isok) CYCLE
+
+  ! CALL nbcptr%GetParam(isSelectionByNodeNum=isok)
+  ! IF (.NOT. isok) CYCLE
+
+  CALL nbcptr%Get(nodeNum=nodeNum, tsize=tsize, fedof=fedof)
+
+  CALL nbcptr%Get(fedof=fedof, nodeNum=nodeNum(1:tsize), &
+                  nodalValue=nodalValue, nrow=ii, ncol=jj)
+
+  CALL rhs%Set(globalNode=nodeNum(1:ii), VALUE=nodalValue(1:ii, 1), &
+               scale=defaultOpt%one, addContribution=defaultOpt%yes, &
+               islocal=defaultOpt%yes)
+END DO
+
+NULLIFY (nbcptr)
+DEALLOCATE (nodalValue, nodeNum)
+
+#ifdef DEBUG_VER
+CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                        '[END] ')
+#endif
+
+END PROCEDURE ScalarFieldAssemblePointSource1
+
+!----------------------------------------------------------------------------
+!                                                             Include Errors
+!----------------------------------------------------------------------------
+
+#include "../../include/errors.F90"
+
+END SUBMODULE Methods
