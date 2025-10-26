f2de8ee2fc2bcda678e3d70719f2a3e16d695cb7
Vikas Sharma <vickysharma0812@gmail.com>
2025-10-26 10:53:54 +0900

    Updating Errors

--- src/submodules/include/errors.F90
+++ src/submodules/include/errors.F90
@@ -20,14 +20,15 @@ END SUBROUTINE AssertError1
 !----------------------------------------------------------------------------
 
 SUBROUTINE AssertError2(a, b, myName, msg)
+  USE Display_Method, ONLY: ToString
   INTEGER(I4B), INTENT(IN) :: a, b
   CHARACTER(*), INTENT(IN) :: myName
   CHARACTER(*), INTENT(IN) :: msg
 
   IF (a .NE. b) THEN
     CALL e%RaiseError(modName//'::'//myName//" - "// &
-  '[INTERNAL ERROR] :: Size error, '//msg//" found a="//tostring(a)//" b = " &
-                      //tostring(b))
+  '[INTERNAL ERROR] :: Size error, '//msg//" found a="//ToString(a)//" b = " &
+                      //ToString(b))
     RETURN
   END IF
 
