24e2e19e6cbb61d3c5c482bcee96d7477b34c2d6
Vikas Sharma <vickysharma0812@gmail.com>
2025-11-13 08:43:19 +0900

    Updating STVectorFieldLis

--- src/submodules/STVectorFieldLis/src/STVectorFieldLis_Class@IOMethods.F90
+++ src/submodules/STVectorFieldLis/src/STVectorFieldLis_Class@IOMethods.F90
@@ -16,15 +16,8 @@
 !
 
 SUBMODULE(STVectorFieldLis_Class) IOMethods
-USE String_Class, ONLY: String
 USE AbstractNodeField_Class, ONLY: AbstractNodeFieldGetPointer, &
-                                   AbstractNodeFieldDisplay, &
-                                   AbstractNodeFieldExport, &
-                                   AbstractNodeFieldImport
-
-USE STVectorField_Class, ONLY: STVectorFieldDisplay, &
-                               STVectorFieldExport, &
-                               SetSTVectorFieldParam
+                                   AbstractNodeFieldDisplay
 
 IMPLICIT NONE
 
@@ -57,130 +50,6 @@ NULLIFY (realvec)
 CALL AbstractNodeFieldDisplay(obj=obj, msg=msg, unitno=unitno)
 
 END PROCEDURE obj_Display
-
-!----------------------------------------------------------------------------
-!                                                                     Import
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE obj_Import
-CHARACTER(*), PARAMETER :: myName = "obj_Import()"
-TYPE(String) :: dsetname
-LOGICAL(LGT) :: bools(3), isok
-TYPE(ParameterList_) :: param
-INTEGER(I4B) :: ierr
-REAL(DFP), POINTER :: realvec(:)
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START] ')
-#endif
-
-CALL AbstractNodeFieldImport(obj=obj, hdf5=hdf5, group=group, &
-                             fedof=fedof, fedofs=fedofs)
-
-! spaceCompo
-dsetname = TRIM(group)//"/spaceCompo"
-isok = hdf5%pathExists(dsetname%chars())
-IF (.NOT. isok) THEN
-  CALL e%RaiseError(modName//'::'//myName//" - "// &
-               '[INTERNAL ERROR] :: The dataset spaceCompo should be present')
-END IF
-CALL hdf5%READ(dsetname=dsetname%chars(), vals=obj%spaceCompo)
-
-! timeCompo
-dsetname = TRIM(group)//"/timeCompo"
-isok = hdf5%pathExists(dsetname%chars())
-IF (.NOT. isok) THEN
-  CALL e%RaiseError(modName//'::'//myName//" - "// &
-                '[INTERNAL ERROR] :: The dataset timeCompo should be present')
-END IF
-CALL hdf5%READ(dsetname=dsetname%chars(), vals=obj%timeCompo)
-
-dsetname = TRIM(group)//"/tSize"
-bools(1) = hdf5%pathExists(dsetname%chars())
-dsetname = TRIM(group)//"/dof"
-bools(2) = hdf5%pathExists(dsetname%chars())
-dsetname = TRIM(group)//"/realVec"
-bools(3) = hdf5%pathExists(dsetname%chars())
-
-isok = ALL(bools)
-
-IF (.NOT. isok) THEN
-
-  CALL param%initiate()
-
-  CALL SetSTVectorFieldParam(param=param, name=obj%name%chars(), &
-                           fieldType=obj%fieldType, timeCompo=obj%timeCompo, &
-                         spaceCompo=obj%spaceCompo, engine=obj%engine%chars())
-
-  obj%isInit = .FALSE.
-
-  CALL obj%Initiate(param=param, fedof=fedof, geofedof=geofedof)
-  CALL param%DEALLOCATE()
-
-  CALL finishMe
-  RETURN
-END IF
-
-! till now we have read tSize, dof, and realVec
-! but we have not created lis vector
-! so we create lis_vector
-! then we update values from realvec to lis_vector using scatter methods
-
-CALL lis_vector_create(obj%comm, obj%lis_ptr, ierr)
-CALL CHKERR(ierr)
-
-CALL lis_vector_set_size(obj%lis_ptr, obj%local_n, obj%global_n, ierr)
-CALL CHKERR(ierr)
-
-CALL lis_vector_get_range(obj%lis_ptr, obj%is, obj%ie, ierr)
-CALL CHKERR(ierr)
-
-realvec => AbstractNodeFieldGetPointer(obj)
-CALL lis_vector_scatter(realvec, obj%lis_ptr, ierr)
-CALL CHKERR(ierr)
-NULLIFY (realvec)
-
-CALL finishMe
-
-CONTAINS
-SUBROUTINE finishMe
-
-  CALL e%RaiseInformation(modName//"::"//myName//" - "// &
-                          "[END]")
-END SUBROUTINE finishMe
-
-END PROCEDURE obj_Import
-
-!----------------------------------------------------------------------------
-!                                                                     Export
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE obj_Export
-CHARACTER(*), PARAMETER :: myName = "obj_Export()"
-INTEGER(I4B) :: ierr
-REAL(DFP), POINTER :: realvec(:)
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[START] ')
-#endif
-
-CALL lis_vector_is_null(obj%lis_ptr, ierr)
-IF (ierr .EQ. LIS_FALSE) THEN
-  realvec => AbstractNodeFieldGetPointer(obj)
-  CALL lis_vector_gather(obj%lis_ptr, realvec, ierr)
-  CALL CHKERR(ierr)
-  NULLIFY (realvec)
-  CALL STVectorFieldExport(obj=obj, hdf5=hdf5, group=group)
-END IF
-
-#ifdef DEBUG_VER
-CALL e%RaiseInformation(modName//'::'//myName//' - '// &
-                        '[END] ')
-#endif
-
-END PROCEDURE obj_Export
 
 !----------------------------------------------------------------------------
 !
