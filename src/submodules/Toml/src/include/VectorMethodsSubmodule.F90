78ad66b22deed62ea2a493cbb52af47c4afe1485
Vikas Sharma <vickysharma0812@gmail.com>
2026-01-24 22:29:36 +0900

    Updating VectorMethodsSubmodules

--- src/submodules/Toml/src/include/VectorMethodsSubmodule.F90
+++ src/submodules/Toml/src/include/VectorMethodsSubmodule.F90
@@ -291,7 +291,7 @@ SUBROUTINE GetVectorValueFromCSV(VALUE, isFound, isScalar, filename)
   CHARACTER(*), PARAMETER :: myName = "GetVectorValueFromCSV()"
 #endif
   TYPE(CSVFile_) :: afile
-  INTEGER(I4B) :: nrow, ncol
+  INTEGER(I4B) :: ncol, version, fileRow, fileCol
 
 #ifdef DEBUG_VER
   CALL e%RaiseInformation(modName//'::'//myName//' - '// &
@@ -305,6 +305,10 @@ SUBROUTINE GetVectorValueFromCSV(VALUE, isFound, isScalar, filename)
                       action="READ", status="OLD", &
                       delimiter=",", comment="#")
   CALL afile%OPEN()
+
+  ! read the meta data of the file
+  CALL GetMetaDataFromCSV(afile, version, fileRow, fileCol)
+
   CALL afile%READ()
   ncol = afile%Getncols()
 
@@ -332,6 +336,207 @@ SUBROUTINE GetVectorValueFromCSV(VALUE, isFound, isScalar, filename)
                           '[END] ')
 #endif
 END SUBROUTINE GetVectorValueFromCSV
+
+!----------------------------------------------------------------------------
+!                                                         GetMetaDataFromCSV
+!----------------------------------------------------------------------------
+
+SUBROUTINE GetMetaDataFromCSV(afile, version, nrow, ncol)
+  TYPE(CSVFile_), INTENT(INOUT) :: afile
+  INTEGER(I4B), INTENT(OUT) :: version
+  INTEGER(I4B), INTENT(OUT) :: nrow
+  INTEGER(I4B), INTENT(OUT) :: ncol
+
+#ifdef DEBUG_VER
+  CHARACTER(*), PARAMETER :: myName = "GetMetaDataFromCSV()"
+  CHARACTER(*), PARAMETER :: three_dashes = "---", &
+                             version_char = "version", &
+                             nrow_char = "nrow", &
+                             ncol_char = "ncol"
+  CHARACTER(3) :: dashes
+#endif
+
+  CHARACTER(*), PARAMETER :: sep = ",", colon = ":"
+  TYPE(String) :: astr
+  TYPE(String), ALLOCATABLE :: tokens(:), two_tokens(:)
+  INTEGER(I4B) :: iostat
+  CHARACTER(1024) :: iomsg
+  LOGICAL(LGT) :: isok
+  INTEGER(I4B) :: ii, tsize
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                          '[START] ')
+#endif
+
+#ifdef DEBUG_VER
+  CALL e%RaiseDebug(modName//'::'//myName//' - '// &
+                    'Reading first line of csv file metadata...')
+#endif
+
+! skip line 1
+  CALL afile%ReadLine(val=astr, iostat=iostat, iomsg=iomsg)
+
+#ifdef DEBUG_VER
+  isok = .NOT. afile%IsEOF()
+  CALL AssertError1(isok, myName, &
+                    "It seems the csvfile has reached end of file")
+#endif
+
+#ifdef DEBUG_VER
+  isok = iostat .LE. 0
+  CALL AssertError1(isok, myName, &
+                    "error while reading csvfile, iomsg: "//TRIM(iomsg))
+#endif
+
+#ifdef DEBUG_VER
+  dashes(1:3) = astr%slice(2, 4)
+  isok = dashes .EQ. three_dashes
+  CALL AssertError1(isok, myName, &
+    "first line in csvfile should have three dashes ---, but it has "//dashes)
+#endif
+
+#ifdef DEBUG_VER
+  CALL e%RaiseDebug(modName//'::'//myName//' - '// &
+                    'Reading second line of csv file metadata...')
+#endif
+
+! read line 2, and process it
+  CALL afile%ReadLine(val=astr, iostat=iostat, iomsg=iomsg, &
+                      ignoreComment=math%no, ignoreBlank=math%no)
+
+#ifdef DEBUG_VER
+  dashes(1:1) = astr%slice(1, 1)
+  isok = dashes(1:1) .EQ. "#"
+  CALL AssertError1(isok, myName, &
+                    "Second line should be a comment starting with #")
+#endif
+
+#ifdef DEBUG_VER
+  ii = astr%SCAN(set=version_char)
+  isok = ii .NE. math%zero_i
+  CALL AssertError1(isok, myName, version_char//" not found in line 2")
+#endif
+
+#ifdef DEBUG_VER
+  ii = astr%SCAN(set=nrow_char)
+  isok = ii .NE. math%zero_i
+  CALL AssertError1(isok, myName, nrow_char//" not found in line 2")
+#endif
+
+#ifdef DEBUG_VER
+  ii = astr%SCAN(set=ncol_char)
+  isok = ii .NE. math%zero_i
+  CALL AssertError1(isok, myName, ncol_char//" not found in line 2")
+#endif
+
+  CALL astr%split(tokens=tokens, sep=sep)
+  tsize = SIZE(tokens)
+
+#ifdef DEBUG_VER
+  isok = tsize .GE. 3
+  CALL AssertError1(isok, myName, &
+                    "version, nrow, ncol should be present in line 3")
+#endif
+
+! handle token 1
+  CALL tokens(1)%split(tokens=two_tokens, sep=colon)
+
+#ifdef DEBUG_VER
+  astr = two_tokens(1)%slice(2, two_tokens(1)%LEN())
+  two_tokens(1) = astr%ADJUSTL()
+  astr = two_tokens(1)%TRIM()
+
+  isok = astr//"" == version_char
+  CALL AssertError1(isok, myName, &
+                    "first of tokens(1) should be version, but it is "//astr)
+#endif
+
+#ifdef DEBUG_VER
+  isok = two_tokens(2)%is_integer()
+  CALL AssertError1(isok, myName, &
+     "second of token(1) should be integer number, but it is "//two_tokens(2))
+#endif
+
+  version = two_tokens(2)%to_number(ii)
+
+! handle token 2
+  CALL tokens(2)%split(tokens=two_tokens, sep=colon)
+
+#ifdef DEBUG_VER
+  astr = two_tokens(1)%ADJUSTL()
+  two_tokens(1) = astr%TRIM()
+  isok = two_tokens(1)//"" == nrow_char
+  CALL AssertError1(isok, myName, &
+                "first of tokens(2) should be nrow, but it is"//two_tokens(1))
+#endif
+
+#ifdef DEBUG_VER
+  isok = two_tokens(2)%is_integer()
+  CALL AssertError1(isok, myName, &
+     "second of token(2) should be integer number, but it is "//two_tokens(2))
+#endif
+
+  nrow = two_tokens(2)%to_number(ii)
+
+! handle token 3
+  CALL tokens(3)%split(tokens=two_tokens, sep=colon)
+
+#ifdef DEBUG_VER
+  astr = two_tokens(1)%ADJUSTL()
+  two_tokens(1) = astr%TRIM()
+  isok = two_tokens(1)//"" == ncol_char
+  CALL AssertError1(isok, myName, &
+                "first of tokens(3) should be ncol, but it is"//two_tokens(1))
+#endif
+
+#ifdef DEBUG_VER
+  isok = two_tokens(2)%is_integer()
+  CALL AssertError1(isok, myName, &
+    "second of tokens(3) should be integer number, but it is "//two_tokens(2))
+#endif
+
+  ncol = two_tokens(2)%to_number(ii)
+
+#ifdef DEBUG_VER
+  CALL e%RaiseDebug(modName//'::'//myName//' - '// &
+                    'Reading third line of csv file metadata')
+#endif
+
+! skip line 3
+  CALL afile%ReadLine(val=astr, iostat=iostat, iomsg=iomsg)
+
+#ifdef DEBUG_VER
+  isok = .NOT. afile%IsEOF()
+  CALL AssertError1(isok, myName, &
+                    "It seems the csvfile has reached end of file")
+#endif
+
+#ifdef DEBUG_VER
+  isok = iostat .LE. 0
+  CALL AssertError1(isok, myName, &
+                    "error while reading csvfile, iomsg: "//TRIM(iomsg))
+#endif
+
+#ifdef DEBUG_VER
+  dashes(1:3) = astr%slice(2, 4)
+  isok = dashes .EQ. three_dashes
+  CALL AssertError1(isok, myName, &
+    "first line in csvfile should have three dashes ---, but it has "//dashes)
+#endif
+
+#ifdef DEBUG_VER
+  CALL e%RaiseDebug(modName//'::'//myName//' - '// &
+                    'Reading second line of csv file metadata...')
+#endif
+
+  DEALLOCATE (tokens, two_tokens)
+
+#ifdef DEBUG_VER
+  CALL e%RaiseInformation(modName//'::'//myName//' - '// &
+                          '[END] ')
+#endif
+END SUBROUTINE GetMetaDataFromCSV
 
 !----------------------------------------------------------------------------
 !                                                    GetVectorValueFromCSV1
