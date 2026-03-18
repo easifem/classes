2d2cf38751c9bd050511625b94e0fe68a7bb4e81
Vikas Sharma <vickysharma0812@gmail.com>
2026-03-18 11:44:30 +0900

    Split GmshModelOccMesh methods into submodule
    Move the SetSize implementation out of the main module into a new submodule file (GmshModelOccMesh_Class@Methods.F90). Introduce an obj_SetSize interface and rename the bound procedure accordingly, expose the type and pointer symbols as PUBLIC, and add debug/info logging and an error include in the new submodule. The implementation now calls GmshModelOccMeshSetSize with SIZE(dimTags, kind=C_SIZE_T) and returns the ierr as INTEGER(I4B). Also remove the previous preprocessor guard and reorganize USE statements for clearer separation of interface and methods.

--- src/modules/GmshAPI/src/GmshModelOccMesh_Class.F90
+++ src/modules/GmshAPI/src/GmshModelOccMesh_Class.F90
@@ -15,59 +15,63 @@
 ! along with this program.  If not, see <https: //www.gnu.org/licenses/>
 !
 
-#ifdef USE_GMSH_SDK
 MODULE GmshModelOccMesh_Class
 USE GlobalData, ONLY: DFP, I4B, LGT
-USE GmshInterface
-USE ISO_C_BINDING
 IMPLICIT NONE
+
 PRIVATE
-CHARACTER( LEN = * ), PARAMETER :: modName = "GMSHMODELOCCMESH_CLASS"
-INTEGER( C_INT ) :: ierr
-!$OMP THREADPRIVATE(ierr)
-INTEGER( I4B ), PARAMETER :: maxStrLen = 256
-
-!----------------------------------------------------------------------------
-!
-!----------------------------------------------------------------------------
-
-TYPE :: GmshModelOccMesh_
-  CONTAINS
-  PRIVATE
-  PROCEDURE, PUBLIC, PASS( Obj ) :: SetSize=>mesh_SetSize
-END TYPE GmshModelOccMesh_
-
 PUBLIC :: GmshModelOccMesh_
-TYPE( GmshModelOccMesh_ ), PUBLIC, PARAMETER :: TypeGmshModelOccMesh = GmshModelOccMesh_()
-
-!----------------------------------------------------------------------------
-!
-!----------------------------------------------------------------------------
-
-TYPE :: GmshModelOccMeshPointer_
-  CLASS( GmshModelOccMesh_ ), POINTER :: Ptr => NULL()
-END TYPE
-
+PUBLIC :: TypeGmshModelOccMesh
 PUBLIC :: GmshModelOccMeshPointer_
 
 !----------------------------------------------------------------------------
 !
 !----------------------------------------------------------------------------
 
+TYPE :: GmshModelOccMesh_
 CONTAINS
+  PRIVATE
+  PROCEDURE, PUBLIC, PASS(Obj) :: SetSize => obj_SetSize
+END TYPE GmshModelOccMesh_
 
-FUNCTION mesh_SetSize(obj, dimTags, meshSize) &
-  & RESULT( ans )
-  CLASS( GmshModelOccMesh_ ), INTENT( INOUT ) :: obj
-  INTEGER( I4B ), INTENT( IN ) :: dimTags( : )
-  REAL( DFP ), INTENT( IN ) :: meshSize
-  INTEGER( I4B ) :: ans
+!----------------------------------------------------------------------------
+!                                                       TypeGmshModelOccMesh
+!----------------------------------------------------------------------------
 
-  ! Internal
-  CALL gmshModelOccMeshSetSize(dimTags, size(dimTags, kind=c_size_t), &
-    & meshSize, ierr)
-  ans = int(ierr, i4b)
-END FUNCTION mesh_SetSize
+TYPE(GmshModelOccMesh_), PARAMETER :: TypeGmshModelOccMesh = &
+                                      GmshModelOccMesh_()
+
+!----------------------------------------------------------------------------
+!                                                    GmshModelOccMeshPointer
+!----------------------------------------------------------------------------
+
+TYPE :: GmshModelOccMeshPointer_
+  CLASS(GmshModelOccMesh_), POINTER :: Ptr => NULL()
+END TYPE GmshModelOccMeshPointer_
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2026-03-18
+! summary: gmsh model occ mesh
+!
+!# SetSize
+!
+! Set size of mesh.
+!
+INTERFACE
+  MODULE FUNCTION obj_SetSize(obj, dimTags, meshSize) RESULT(ans)
+    CLASS(GmshModelOccMesh_), INTENT(INOUT) :: obj
+    INTEGER(I4B), INTENT(IN) :: dimTags(:)
+    REAL(DFP), INTENT(IN) :: meshSize
+    INTEGER(I4B) :: ans
+  END FUNCTION obj_SetSize
+END INTERFACE
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END MODULE GmshModelOccMesh_Class
-#endif
