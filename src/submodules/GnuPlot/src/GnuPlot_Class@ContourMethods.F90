9317889008821e79f1f3067b3648b3a8247fd641
shion <shimizu.shion819@gmail.com>
2026-05-22 16:09:31 +0900

    Updates in Gnuplot_Class
    - add new methods
    - improve performance

--- src/submodules/GnuPlot/src/GnuPlot_Class@ContourMethods.F90
+++ src/submodules/GnuPlot/src/GnuPlot_Class@ContourMethods.F90
@@ -26,47 +26,25 @@ CONTAINS
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE obj_Contour1
-TYPE(String) :: pltstring
 CHARACTER(*), PARAMETER :: datablock = '$xyz'
-LOGICAL(LGT) :: fill0
-CHARACTER(:), ALLOCATABLE :: paletteName0
 
 obj%opts%plotOpts%datastyle = 'lines'
+
 CALL obj%Initiate()
+
 CALL obj%WritePlotSetup()
 
 CALL Help_WriteDataBlock_xyz(obj, x, y, z, blockName=datablock)
 
-CALL obj%pltfile%WriteBlank()
-CALL obj%pltfile%WRITE('# create the contour')
-CALL obj%pltfile%WRITE('set contour base')
+CALL Help_WriteCntrBasicOptions(obj)
 
-fill0 = Input(default=obj%opts%plotOpts%fill, option=fill)
-IF (fill0) CALL obj%pltfile%WRITE('set contourfill cbtics')
+CALL Help_SetCntrFill(obj, fill)
 
-CALL obj%pltfile%WRITE('set cntrparam levels '// &
-                       tostring(obj%opts%plotOpts%numLevels))
+CALL Help_SetCntrLevels(obj)
 
-CALL obj%pltfile%WRITE('unset surface')
-CALL obj%pltfile%WRITE('set view map')
+CALL Help_WritePaletteInfo(obj, paletteName)
 
-paletteName0 = Input(default=obj%opts%plotOpts%paletteName%chars(), &
-                     option=paletteName)
-IF (LEN(paletteName0) .GT. 0) THEN
-  CALL obj%pltfile%WRITE(GetColorPaletteScript(paletteName0))
-  CALL obj%pltfile%WRITE('set pm3d')
-END IF
-
-CALL obj%pltfile%WriteBlank()
-
-pltstring = ''
-IF (PRESENT(lspec)) THEN
-  pltstring = "splot "//datablock//' '//TRIM(lspec)
-ELSE
-  pltstring = 'splot '//datablock
-END IF
-
-CALL obj%pltfile%WRITE(pltstring%chars())
+CALL Help_WriteSplotString(obj, datablock, lspec)
 
 CALL obj%DEALLOCATE()
 
@@ -104,13 +82,8 @@ END PROCEDURE obj_Contour2
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE obj_Contour3
-INTEGER(I4B) :: ncx, nrx, ii, jj
-LOGICAL(LGT) :: xyz_data
-TYPE(String) :: pltstring
-CHARACTER(*), PARAMETER :: datablock1 = '$xyz', &
+CHARACTER(*), PARAMETER :: datablock1 = '$xyz1', &
                            datablock2 = '$xyz2'
-LOGICAL(LGT) :: fill0
-CHARACTER(:), ALLOCATABLE :: paletteName0
 
 obj%opts%plotOpts%datastyle = 'lines'
 CALL obj%Initiate()
@@ -120,50 +93,126 @@ CALL Help_WriteDataBlock_xyz(obj, x1, y1, z1, blockName=datablock1)
 CALL obj%pltfile%WriteBlank()
 
 CALL Help_WriteDataBlock_xyz(obj, x2, y2, z2, blockName=datablock2)
-
 CALL obj%pltfile%WriteBlank()
 
-CALL obj%pltfile%WRITE('# create the contour')
-CALL obj%pltfile%WRITE('set contour base')
+CALL Help_WriteCntrBasicOptions(obj)
 
-fill0 = Input(default=obj%opts%plotOpts%fill, option=fill)
-IF (fill0) CALL obj%pltfile%WRITE('set contourfill cbtics')
+CALL Help_SetCntrFill(obj, fill)
 
-CALL obj%pltfile%WRITE('set cntrparam levels 14')
+CALL Help_SetCntrLevels(obj)
 
-CALL obj%pltfile%WRITE('unset surface')
-CALL obj%pltfile%WRITE('set view map')
+CALL Help_WritePaletteInfo(obj, paletteName)
 
-paletteName0 = Input(default=obj%opts%plotOpts%paletteName%chars(), &
-                     option=paletteName)
-IF (LEN(paletteName0) .GT. 0) THEN
-  CALL obj%pltfile%WRITE(GetColorPaletteScript(paletteName0))
-  CALL obj%pltfile%WRITE('set pm3d')
-END IF
+CALL Help_WriteSplotString(obj, datablock1, lspec1, append=.TRUE.)
 
-CALL obj%pltfile%WriteBlank()
-
-IF (PRESENT(lspec1)) THEN
-  pltstring = "splot "//datablock1//' '//TRIM(lspec1)
-ELSE
-  pltstring = 'splot '//datablock1
-END IF
-pltstring = pltstring//", "//CHAR_BSLASH
-
-CALL obj%pltfile%WRITE(pltstring%chars())
-
-IF (PRESENT(lspec2)) THEN
-  pltstring = "splot "//datablock2//' '//TRIM(lspec2)
-ELSE
-  pltstring = 'splot '//datablock1
-END IF
-pltstring = pltstring//", "//CHAR_BSLASH
-
-CALL obj%pltfile%WRITE(pltstring%chars())
+CALL Help_WriteSplotString(obj, datablock1, lspec1, append=.FALSE.)
 
 CALL obj%DEALLOCATE()
 
 END PROCEDURE obj_Contour3
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBROUTINE Help_SetCntrLevels(obj)
+  CLASS(GnuPlot_), INTENT(INOUT) :: obj
+
+  INTEGER(I4B) :: ilevel
+
+  IF (obj%opts%splotOpts%discreteLevel) THEN
+    CALL obj%pltfile%WRITE('set cntrparam levels discrete ', &
+                           advance="NO")
+    DO ilevel = 1, SIZE(obj%opts%splotOpts%levels) - 1
+      CALL obj%pltfile%WRITE(tostring(obj%opts%splotOpts%levels(ilevel)) &
+                             //",", advance="NO")
+    END DO
+    CALL obj%pltfile%WRITE(tostring(obj%opts%splotOpts%levels(ilevel)))
+  ELSE
+    CALL obj%pltfile%WRITE('set cntrparam levels '// &
+                           tostring(obj%opts%splotOpts%numLevels))
+  END IF
+
+END SUBROUTINE Help_SetCntrLevels
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBROUTINE Help_SetCntrFill(obj, fill)
+  CLASS(GnuPlot_), INTENT(INOUT) :: obj
+  LOGICAL(LGT), OPTIONAL, INTENT(IN) :: fill
+
+  LOGICAL(LGT) :: fill0
+
+  fill0 = Input(default=obj%opts%splotOpts%fill, option=fill)
+  IF (fill0) CALL obj%pltfile%WRITE('set contourfill cbtics')
+
+END SUBROUTINE Help_SetCntrFill
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBROUTINE Help_WriteCntrBasicOptions(obj)
+  CLASS(GnuPlot_), INTENT(INOUT) :: obj
+
+  CALL obj%pltfile%WriteBlank()
+  CALL obj%pltfile%WRITE('# create the contour')
+  CALL obj%pltfile%WRITE('set contour base')
+  CALL obj%pltfile%WRITE('unset surface')
+  CALL obj%pltfile%WRITE('set view map')
+
+END SUBROUTINE Help_WriteCntrBasicOptions
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBROUTINE Help_WritePaletteInfo(obj, paletteName)
+  CLASS(GnuPlot_), INTENT(INOUT) :: obj
+  CHARACTER(*), OPTIONAL, INTENT(IN) :: paletteName
+
+  CHARACTER(:), ALLOCATABLE :: paletteName0
+
+  paletteName0 = Input(default=obj%opts%splotOpts%paletteName%chars(), &
+                       option=paletteName)
+
+  IF (LEN(paletteName0) .GT. 0) THEN
+    CALL obj%pltfile%WRITE(GetColorPaletteScript(paletteName0))
+    CALL obj%pltfile%WRITE('set pm3d')
+  END IF
+
+  CALL obj%pltfile%WriteBlank()
+
+END SUBROUTINE Help_WritePaletteInfo
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
+
+SUBROUTINE Help_WriteSplotString(obj, dataBlock, lspec, append)
+  CLASS(GnuPlot_), INTENT(INOUT) :: obj
+  CHARACTER(*), INTENT(IN) :: dataBlock
+  CHARACTER(*), OPTIONAL, INTENT(IN) :: lspec
+  LOGICAL(LGT), OPTIONAL, INTENT(IN) :: append
+
+  LOGICAL(LGT) :: append0
+  TYPE(String) :: pltString
+
+  append0 = Input(default=.FALSE., option=append)
+
+  IF (PRESENT(lspec)) THEN
+    pltString = "splot "//dataBlock//' '//TRIM(lspec)
+  ELSE
+    pltString = "splot "//dataBlock
+  END IF
+
+  IF (append0) pltString = pltString//", "//CHAR_BSLASH
+
+  CALL obj%pltfile%WRITE(pltString%chars())
+
+END SUBROUTINE Help_WriteSplotString
 
 !----------------------------------------------------------------------------
 !
